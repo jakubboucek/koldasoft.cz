8b5e4e13c437c1d6c337abfa950bcb576cb14751
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-01-26 12:26:14 +0000

    Oprava referencí.
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@10 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/reference.php
+++ source/pages/reference.php
@@ -263,13 +263,13 @@
             <div class=\"reference\">
               <div class=\"text\">
                 <h3>Auta Míra</h3>
-                <p class=\"link\"><a href=\"http://www.auta-mira.cz/\" title=\"Přejít na stránky Auta Míra\">www.auta-mira.cz</a></p>
+                <p class=\"link\"><a href=\"".$cesta."reference/auta_mira.png\" title=\"Zobrazit velký náhled\">www.auta-mira.cz</a></p>
                 <p class=\"short\"><strong>Kompletní realizace</strong>, <strong><a href=\"#logotyp_auta_mira\" title=\"Reference logotypu Auta-Míra\">logotyp</a></strong>, červen&nbsp;2007</p>
                 <p class=\"long\">Aktuální nabídka ojetých vozů autobazaru Auta-Míra. Nabízené vozy, jejich popis a fotogalerii si spravuje sám majitel autobazaru prostřednictvím jednoduchého administračního rozhraní. Při tvorbě stránek jsme se zaměřili na jednoduché ovládání a přehlednost.</p>
                 <p class=\"read_next\"><a href=\"#\" title=\"Více informací o projektu Auta Míra\">Více o tomto projektu</a></p>
               </div>
               <div class=\"foto\">
-                <p><a href=\"http://www.auta-mira.cz/\" title=\"Přejít na stránky Auta Míra\"><img src=\"".$cesta."reference/auta_mira_200.png\" alt =\"Náhled: projekt Auta Míra\" width=\"200\" height=\"200\" /></a></p>
+                <p><a href=\"".$cesta."reference/auta_mira.png\" title=\"Zobrazit velký náhled\"><img src=\"".$cesta."reference/auta_mira_200.png\" alt =\"Náhled: projekt Auta Míra\" width=\"200\" height=\"200\" /></a></p>
               </div>  
               <div class=\"cleaner\"></div>
             </div>
