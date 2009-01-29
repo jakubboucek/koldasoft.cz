137b514114f686f006d859aac6570043bd05e9ea
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-01-29 15:49:34 +0000

    Oprava překlepu.
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@11 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/reference.php
+++ source/pages/reference.php
@@ -194,7 +194,7 @@
                 <p class=\"link\"><a href=\"http://www.fa-krystal.cz/nase-sluzby/\" title=\"Přejít na stránky Krystal\">www.fa-krystal.cz</a></p>
                 <p class=\"short\"><strong>Redesign, Kompletní realizace</strong>, duben&nbsp;2008</p>
                 <p class=\"long\">Webové stránky středního rozsahu, poskytující informace o základní nabídce služeb, která je rozdělena na čtyři části oblasti působení.</p>
-                <p class=\"long\">Cílem redesignu bylo maximaliziovat prestiž firmy na internetu, čehož jsme dosáhli poutavým grafickým zpracováním a kvalitním technickým řešením.</p>
+                <p class=\"long\">Cílem redesignu bylo maximalizovat prestiž firmy na internetu, čehož jsme dosáhli poutavým grafickým zpracováním a kvalitním technickým řešením.</p>
                 <p class=\"read_next\"><a href=\"#\" title=\"Více informací o projektu Krystal\">Více o tomto projektu</a></p>
               </div>
               <div class=\"foto\">
