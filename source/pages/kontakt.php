05e11ef8f9fa0439951e55b1b32e63d1ec933fca
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-11 14:45:14 +0000

    Oprava tučných položek (onaktní údaje).
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@51 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/kontakt.php
+++ source/pages/kontakt.php
@@ -18,10 +18,10 @@
             <h2>Koldasoft,&nbsp;s.r.o.</h2>
             <p>sady&nbsp;Pětatřicátníků&nbsp;48/33 (<a href=\"http://www.mapy.cz/#x=130647296@y=134780992@z=14@mm=ZP@sa=s@st=s@ssq=sady%20P%C4%9Btat%C5%99ic%C3%A1tn%C3%ADk%C5%AF%2048/33,%20Plze%C5%88@sss=1@ssp=130642112_134773584_130671232_134798656\" title=\"Najít adresu na Mapy.cz\">mapa</a>)</p>
             <p>301&nbsp;00 Plzeň &ndash; Jižní Předměstí</p>
-            <p>IČ: <a href=\"http://www.rzp.cz/cgi-bin/aps_cacheWEB.sh?Action=Search&amp;VSS_SERV=ZVWSBJFND&amp;ICO=28009703\" title=\"Zobrazit výpis ze živnostenského rejstříku\"><strong>28009703</strong></a>
-            <p>DIČ: <a href=\"http://adisreg.mfcr.cz/adistc/DphReg?ZPRAC=RDPHI1&amp;id=1&amp;pocet=1&amp;fu=&amp;dic=28009703&amp;OK=+Hledej+\" title=\"Zobrazit výpis z rejstříku pláctů DPH\"><strong>CZ28009703</strong></a>, jsme plátci DPH</p>
-            <p>č.ú.: <strong>43-1580080207/0100</strong> (KB)</p>
-            <p>IBAN: <strong>CZ4501000000431580080207</strong></p>
+            <p><strong>IČ:</strong> <a href=\"http://www.rzp.cz/cgi-bin/aps_cacheWEB.sh?Action=Search&amp;VSS_SERV=ZVWSBJFND&amp;ICO=28009703\" title=\"Zobrazit výpis ze živnostenského rejstříku\">28009703</a>
+            <p><strong>DIČ:</strong> <a href=\"http://adisreg.mfcr.cz/adistc/DphReg?ZPRAC=RDPHI1&amp;id=1&amp;pocet=1&amp;fu=&amp;dic=28009703&amp;OK=+Hledej+\" title=\"Zobrazit výpis z rejstříku pláctů DPH\">CZ28009703</a>, jsme plátci DPH</p>
+            <p><strong>č.ú.:</strong> 43-1580080207/0100 (KB)</p>
+            <p><strong>IBAN:</strong> CZ4501000000431580080207</p>
             <p>Obchodní společnost s ručením omezeným je zapsána u Krajského soudu v Plzni,<br />oddíl&nbsp;C, vložka&nbsp;21277 (<a href=\"http://wwwinfo.mfcr.cz/cgi-bin/ares/ares_es.cgi?jazyk=cz&amp;ico=28009703&amp;cestina=cestina&amp;maxpoc=200&amp;setrid=ZADNE&amp;xml=1\" title=\"Zobrazit výpis z obchodního rejstříku\">ARES</a>).</p>
             
             <img style=\"float: right;\" src=\"".$cesta."pictures/img.png\" alt=\"foto magnum\" width=\"200\" height=\"150\" />
@@ -31,9 +31,9 @@
             <p>530&nbsp;02 Pardubice &ndash; Zelené Předměstí</p>
             <p>Budova MAGNUM, 6. patro</p>
             <h3>Další kontaktní informace</h3>
-            <p>Telefon: <strong>775&nbsp;77&nbsp;15&nbsp;00</strong>&nbsp;(".mylink("nas-tym", "Jiří Kolařík", "kolda", "Jiří Kolařík").", ředitel) a <strong>775&nbsp;77&nbsp;15&nbsp;10</strong>&nbsp;(".mylink("nas-tym", "Lucie Brandýská", "lucka", "Lucie Brandýská").", asistentka)</p>
-            <p>E-mail: <a href=\"mailto:info@koldasoft.cz\" title=\"Poslat E-mail\"><strong>info@koldasoft.cz</strong></a></p>
-            <p>Webové stránky: <a href=\"http://www.koldasoft.cz/\" title=\"Přejít na webové stránky společnosti Koldasoft, s.r.o.\"><strong>www.koldasoft.cz</strong></a></p>
+            <p><strong>Telefon:</strong> 775&nbsp;77&nbsp;15&nbsp;00&nbsp;(".mylink("nas-tym", "Jiří Kolařík", "kolda", "Jiří Kolařík").", ředitel) a 775&nbsp;77&nbsp;15&nbsp;10&nbsp;(".mylink("nas-tym", "Lucie Brandýská", "lucka", "Lucie Brandýská").", asistentka)</p>
+            <p><strong>E-mail:</strong> <a href=\"mailto:info@koldasoft.cz\" title=\"Poslat E-mail\">info@koldasoft.cz</a></p>
+            <p>Webové stránky: <a href=\"http://www.koldasoft.cz/\" title=\"Přejít na webové stránky společnosti Koldasoft, s.r.o.\">www.koldasoft.cz</a></p>
             <p>Neváhejte také kontaktovat kohokoliv z&nbsp;".mylink("nas-tym", "našeho týmu").".</p>
             <h3>Kontaktní formulář</h3>
           </div>  
