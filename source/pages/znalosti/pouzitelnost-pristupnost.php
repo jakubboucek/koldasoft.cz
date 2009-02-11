c4c58ce348aa3f2f7dab11d5cedd1311e7629345
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-11 21:44:08 +0000

    Upravený obsah sekce "Znalosti".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@75 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/znalosti/pouzitelnost-pristupnost.php
+++ source/pages/znalosti/pouzitelnost-pristupnost.php
@@ -2,13 +2,22 @@
 
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
-            <p>Vaše stránky by měly být k dispozici všem návštěvníkům, potenciálním zákazníkům. ".mylink("pozadavky-kvalitni-stranky", "Kvalitní webové stránky")." musí být stejně dobře použitelné na všech počítačích, zařízeních i pro všechny hendikepované návštěvníky. Návštěvník by měl Vaší stránce plně rozumět, orientovat se v navigaci a celkově se na Vašem webu cítit dobře.</p>
+            <p>Vaše stránky by měly být k&nbsp;dispozici všem návštěvníkům, potenciálním zákazníkům. ".mylink("pozadavky-kvalitni-stranky", "Kvalitní webové stránky")." musí být stejně dobře použitelné na všech počítačích, zařízeních i pro všechny hendikepované návštěvníky. Návštěvník by měl Vaší stránce plně rozumět, orientovat se v&nbsp;navigaci a celkově se na Vašem webu cítit dobře.</p>
           </div>  
           <div class=\"text_box\">
             <h2 id=\"pouzitelnost\">Použitelnost</h2>
-            <p>Je vlastně o tom, jak se budou webové stránky návštěvníkovi <strong>snadno ovládat</strong>, zda bude ovládání intuitivní a jak se bude na stránkách cítit. Návštěvník by měl vždy vědět, na jaké stránce se nachází, jak se vrátit na předchozí stránku, jak se vrátit na úvodní stránku a jak pokračovat&hellip; Menu by mělo být jasné, přehledné a úplné. Prvky na stránkách by měly být vhodně umístěny a neměly by svoji polohu měnit.</p>
+            <p>Návštěvník by měl vždy vědět, na jaké stránce se nachází, jak se vrátit na předchozí stránku, jak se vrátit na úvodní stránku a jak pokračovat&hellip; Menu by mělo být jasné, přehledné a úplné. Prvky na stránkách by měly být vhodně umístěny a neměly by svoji polohu měnit.</p>
+
+
+          <div class=\"float_box question\">
+            <div class=\"title\"><strong>Co je to použitelnost?</strong></div>
+            <p>Použitelnost je vlastně o tom, jak snadno se webové stránky návštěvníkovi ovládají, zda je <strong>ovládání intuitivní</strong> a jak se návštěvník na stránkách cítí.</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
             <p>Pokud se návštěvník bude cítit na Vašich stránkách dobře, <strong>zvýší se tím jeho důvěra</strong> ve Vaši firmu a může se stát zákazníkem. Naopak, pokud bude návštěvník jakkoliv odrazen, nebude si jistý a jeho důvěru nezískáte.</p>
-            <p>Jsou již zaběhlé standardy a symboly, které <strong>uživatelé znají a očekávají</strong>. Například, že se logo firmy vyskytuje v levém horním rohu a je odkazem na úvodní stránku, že odkazy jsou vyznačené, obvykle podtržené, &hellip; Symbolem nákupního košíku se označuje vložení produktu do košíku, obálkou nebo zavináčem adresa elektronické pošty, E-mailu. Pro odebrání (odstranění, zrušení) se používá křížek a pro potvrzení &bdquo;fajfka&ldquo;.</p>
+            <p>Jsou již zaběhlé standardy a symboly, které <strong>uživatelé znají a očekávají</strong>. Například, že se logo firmy vyskytuje v&nbsp;levém horním rohu a je odkazem na úvodní stránku, že odkazy jsou vyznačené, obvykle podtržené, &hellip; Symbolem nákupního košíku se označuje vložení produktu do košíku, obálkou nebo zavináčem adresa elektronické pošty, E-mailu. Pro odebrání (odstranění, zrušení) se používá křížek a pro potvrzení &bdquo;fajfka&ldquo;.</p>
             <h3>Nejčastější chyby použitelnosti</h3>
             <p>Jistě jste se na internetu setkali s některou z níže uvedených chyb. Mohlo Vás to zmást, rozčílit či zklamat:</p>
             <ul>
@@ -18,18 +27,34 @@
               <li><strong>Nepřehlednost</strong> - nepřehledné členění nadpisů, textů a odstavců.</li>
             </ul>
             <h2 id=\"pristupnost\">Přístupnost</h2>
-            <p>Snahou je zpřístupnit webové stránky co nejširší veřejnosti bez ohledu na jejich technické vybavení nebo možný hendikep, který mohou návštěvníci mít.</p>
             <p>Mezi hendikepované uživatele řadíme všechny, kteří <strong>nemohou použít webovou stránku běžným způsobem</strong>. Mohou tedy mít zrakové, sluchové nebo pohybové problémy. Často se však zapomíná, že dočasně hendikepovaným uživatelem může být kdokoliv – např. při ostrém svitu sluníčka na monitor je zhoršena viditelnost, ovládání myši může být velkým problémem, pokud má návštěvník zlomenou ruku, &hellip; Hendikepovaným návštěvníkem tedy může být i ten, který v běžném životě žádný hendikep nemá.</p>
-            <p>Návštěvníci mohou používat různé internetové <acronym>prohlížeče</acronym>, tzv. browsery. K prohlížení webových stránek se dnes používá mnoho zařízení, zejména <acronym>PDA</acronym> nebo mobilní telefony. Nikdy nelze přesně určit, jak bude stránka prezentovaná (obrazovka PC, PDA, projektor či <acronym>hlasová čtečka</acronym>). Práci s webovou stránkou řídí vždy uživatel (nastavení stránky jako domovské, otevření stránky v novém okně, &hellip;).</p>
-            <p>".mylink("pozadavky-kvalitni-stranky", "Kvalitní webové stránky")." <strong>respektují všechny tyto uživatele</strong> a umožňují všem, bez rozdílu, získat shodné informace. Pokud by návštěvník nemohl přečíst či použít Vaše webové stránky, pravděpodobně odejde jinam, ke konkurenci. Tam, kde mu bude umožněno získat všechny informace, a to bez omezení. Je nanejvýše vhodné, aby byla stránka předložena správně vždy a za všech okolností. Zda je webová stránka přístupná (a v jakém rozsahu) musí být uvedeno na každé stránce. Tyto informace se většinou nachází ve spodní části, tzv. patičce.</p>
-            <p>Více informací o přístupnosti můžete získat na adrese: <a href=\"http://pristupnost.nawebu.cz\" title=\"\">http://pristupnost.nawebu.cz</a>.</p>
+
+
+          <div class=\"float_box question\">
+            <div class=\"title\"><strong>Co je to přístupnost?</strong></div>
+            <p>Snahou je zpřístupnit webové stránky co <strong>nejširší veřejnosti</strong> bez ohledu na jejich technické a softwarové vybavení nebo možný hendikep, který mohou návštěvníci mít.</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
+            <p>Návštěvníci mohou používat <strong>různé internetové <acronym>prohlížeče</acronym></strong>, tzv. browsery. K&nbsp;prohlížení webových stránek se dnes používá mnoho zařízení, zejména <acronym>PDA</acronym> nebo mobilní telefony. Nikdy nelze přesně určit, jak bude stránka prezentovaná (obrazovka PC, PDA, projektor či <acronym>hlasová čtečka</acronym>). Práci s&nbsp;webovou stránkou řídí vždy uživatel (nastavení stránky jako domovské, otevření stránky v&nbsp;novém okně, &hellip;).</p>
+            <p>".mylink("pozadavky-kvalitni-stranky", "Kvalitní webové stránky")." <strong>respektují všechny tyto uživatele</strong> a umožňují všem, bez rozdílu, získat shodné informace. Pokud by návštěvník nemohl přečíst či použít Vaše webové stránky, pravděpodobně odejde jinam, ke konkurenci. Tam, kde mu bude umožněno získat všechny informace, a to bez omezení. Je nanejvýše vhodné, aby byla stránka předložena správně vždy a za všech okolností. Zda je webová stránka přístupná (a v&nbsp;jakém rozsahu) musí být uvedeno na každé stránce. Tyto informace se většinou nachází ve spodní části, tzv. patičce (".mylink("prohlaseni-o-pristupnosti", "přístupnost tohoto webu")."). </p>
             <h3>Nejčastější chyby přístupnosti</h3>
-            <p>Jistě jste se na internetu setkali s některou z níže uvedených chyb, která Vás omezila:</p>
+            <p>Jistě jste se na internetu setkali s&nbsp;některou z&nbsp;níže uvedených chyb, která Vás omezila:</p>
+
+
+          <div class=\"float_box tip\">
+            <div class=\"title\"><strong>Další informace</strong></div>
+            <p>Více informací o přístupnosti můžete získat na adrese: <a href=\"http://pristupnost.nawebu.cz\" title=\"\">http://pristupnost.nawebu.cz</a>.</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
             <ul>
               <li><strong>Špatné (nekorektní) zobrazení</strong> – stránka se Vám rozsypala, chyběl její kousek, byla překryta jinou částí apod.</li>
-              <li><strong>Velikost písma</strong> – písmo je příliš malé (velké) a jeho velikost nelze změnit. Velká část uživatelů si pro snažší čitelnost písmo zvětšuje. Pokud nebude zvětšení možné, uživatel si webové stránky nikdy nepřečte.</li>
+              <li><strong>Velikost písma</strong>  písmo je příliš malé (velké) a jeho velikost nelze změnit. Velká část uživatelů si pro snažší čitelnost písmo zvětšuje. Pokud nebude zvětšení možné, uživatel si webové stránky nikdy nepřečte.</li>
               <li><strong>Barevnost</strong> – nejčastěji chybnou volbou barev, nedostatečně kontrastní text vůči pozadí, špatné odlišení zvýrazněných prvků, nevhodná volba pozadí, rušivé elementy.</li>
-              <li><strong>Chybná podpora alternativních prohlížečů</strong> – mnohdy bývají stránky programovány pouze pro Internet Explorer, v alternativních prohlížečích (Mozilla Firefox, Opera, &hellip;) se poté zobrazují nekorektně. I <acronym>vyhledávač</acronym> používá alternativní prohlížeč – textový.</li>
+              <li><strong>Chybná podpora alternativních prohlížečů</strong> – mnohdy bývají stránky programovány pouze pro Internet Explorer, v&nbsp;alternativních prohlížečích (Mozilla Firefox, Opera, &hellip;) se poté zobrazují nekorektně. I <acronym>vyhledávač</acronym> (např. Google) používá alternativní prohlížeč – textový.</li>
             </ul>
           </div>".nl);
 
