c7533421455da89bde652fec53588e5c7fdb5899
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-11 18:59:25 +0000

    Upravený obsah sstránky "Znalosti".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@66 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/znalosti.php
+++ source/pages/znalosti.php
@@ -2,14 +2,24 @@
 
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
+
+
+          <div class=\"float_box tip\">
+            <p class=\"title\"><strong>".mylink("slovnicek-pojmu")."</strong></p>
+            <p>Nabízíme seznam nejpoužívanějších pojmů v&nbsp;oblasti webdesignu&hellip;</p>
+            <p class=\"read_next\">".mylink("nas-tym", "Prohlédněte si slovníček pojmů")."</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
             <p>Za vlastní tvorbou ".mylink("pozadavky-kvalitni-stranky", "kvalitních webových stránek")." je skryta, i když to nemusí být na první pohled patrné, složitá a komplexní práce, vyžadující hluboké znalosti, odbornost a pochopení celé problematiky.</p>
-            <p>Zde uvádíme přehled důležitých znalostí z oblasti webdesignu.</p>
+            <p>Zde uvádíme přehled důležitých znalostí z&nbsp;oblasti webdesignu.</p>
           </div>  
           <div class=\"text_box\">
             <h2 id=\"pouzitelnost-pristupnost\">".mylink("pouzitelnost-pristupnost")."</h2>
-            <p>Vaše stránky by měly být k dispozici všem návštěvníkům, potenciálním zákazníkům. Stránky musí být stejně dobře použitelné na všech počítačích, zařízeních i pro všechny hendikepované uživatele. Návštěvník by měl Vaší webové stránce plně rozumět, orientovat se v navigaci a celkově se na Vašem webu cítit dobře. Pomůžeme Vám takovéto stránky vytvořit. ".mylink("pouzitelnost-pristupnost", "čtěte dále")."</p>
+            <p>Vaše stránky by měly být k&nbsp;dispozici všem návštěvníkům, potenciálním zákazníkům. Stránky musí být stejně dobře použitelné na všech počítačích, zařízeních i pro všechny hendikepované uživatele. Návštěvník by měl Vaší webové stránce plně rozumět, orientovat se v&nbsp;navigaci a celkově se na Vašem webu cítit dobře. Pomůžeme Vám takovéto stránky vytvořit&hellip; ".mylink("pouzitelnost-pristupnost", "čtěte dále")."</p>
             <h2 id=\"metody-seo\">".mylink("metody-seo")."</h2>
-            <p>SEO – optimalizace stránek pro vyhledávače je náročná činnost, která vyžaduje dodržení základních prvků, které jsou dle způsobu aplikování rozděleny na dvě skupiny: on-page a off-page faktory. Nabízíme Vám stručný popis našich metod pro SEO optimalizaci. ".mylink("metody-seo", "čtěte dále")."</p>
+            <p>SEO – optimalizace stránek pro vyhledávače je náročná činnost, která vyžaduje dodržení základních prvků, které jsou dle způsobu aplikování rozděleny na dvě skupiny: on-page a off-page faktory. Nabízíme Vám stručný popis našich metod pro SEO optimalizaci&hellip; ".mylink("metody-seo", "čtěte dále")."</p>
           </div>".nl); 
 
 ?>
