a8ec8bf7359de23663373c9946b8474feb22cb1d
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-11 09:32:12 +0000

    Upravený obsah stránky "Konakt".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@33 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/kontakt.php
+++ source/pages/kontakt.php
@@ -1,50 +1,44 @@
 <?php  //Kontakt
 
   echo("          <div class=\"text_box\">
-            <h1>$page_name</h1>
-            <p>Fakturační a kontaktní údaje obchodní společnosti Koldasoft,&nbsp;s.r.o. Působíme především v Pardubicích, Praze a Plzni.</p>  
+
+          <div class=\"float_box\">
+            <p class=\"title\">T&nbsp;&nbsp;&nbsp;Lidé v týmu</p>
+            <p>Hledáte někoho konkrétního? Potřebujete číslo nebo mailovou adresu? Rádi byste mluvili přímo s grafikem?</p>
+            <p class=\"read_next\">Koukněte na ".mylink("nas-tym", "náš tým")."</p>
           </div>  
+
+
+            <h1>$page_name</h1>
+            <p>Fakturační a kontaktní údaje obchodní společnosti Koldasoft,&nbsp;s.r.o.</p>  
+          </div>
+          
+            
           <div class=\"text_box\">
             <h2>Koldasoft,&nbsp;s.r.o.</h2>
-            <p>sady&nbsp;Pětatřicátníků&nbsp;48/33</p>
+            <p>sady&nbsp;Pětatřicátníků&nbsp;48/33 (<a href=\"http://www.mapy.cz/#x=130647296@y=134780992@z=14@mm=ZP@sa=s@st=s@ssq=sady%20P%C4%9Btat%C5%99ic%C3%A1tn%C3%ADk%C5%AF%2048/33,%20Plze%C5%88@sss=1@ssp=130642112_134773584_130671232_134798656\" title=\"Najít adresu na Mapy.cz\">mapa</a>)</p>
             <p>301&nbsp;00 Plzeň &ndash; Jižní Předměstí</p>
-            <p>IČ: <strong>28009703</strong>
-            <p>DIČ: <strong>CZ28009703</strong>, jsme plátci DPH</p>
+            <p>IČ: <a href=\"http://www.rzp.cz/cgi-bin/aps_cacheWEB.sh?Action=Search&amp;VSS_SERV=ZVWSBJFND&amp;ICO=28009703\" title=\"Zobrazit výpis ze živnostenského rejstříku\"><strong>28009703</strong></a>
+            <p>DIČ: <a href=\"http://adisreg.mfcr.cz/adistc/DphReg?ZPRAC=RDPHI1&amp;id=1&amp;pocet=1&amp;fu=&amp;dic=28009703&amp;OK=+Hledej+\" title=\"Zobrazit výpis z rejstříku pláctů DPH\"><strong>CZ28009703</strong></a>, jsme plátci DPH</p>
             <p>č.ú.: <strong>43-1580080207/0100</strong> (KB)</p>
             <p>IBAN: <strong>CZ4501000000431580080207</strong></p>
-            <p>Obchodní společnost s ručením omezeným je zapsána u Krajského soudu v Plzni, oddíl&nbsp;C, vložka&nbsp;21277.</p>
+            <p>Obchodní společnost s ručením omezeným je zapsána u Krajského soudu v Plzni,<br />oddíl&nbsp;C, vložka&nbsp;21277 (<a href=\"http://wwwinfo.mfcr.cz/cgi-bin/ares/ares_es.cgi?jazyk=cz&amp;ico=28009703&amp;cestina=cestina&amp;maxpoc=200&amp;setrid=ZADNE&amp;xml=1\" title=\"Zobrazit výpis z obchodního rejstříku\">ARES</a>).</p>
+            
+            <img style=\"float: right;\" src=\"".$cesta."pictures/img.png\" alt=\"foto magnum\" width=\"200\" height=\"150\" />
+            
             <h3>Pobočka Pardubice</h3>
-            <p>třída&nbsp;Míru&nbsp;2800</p>
+            <p>třída&nbsp;Míru&nbsp;2800 (<a href=\"http://www.mapy.cz/#x=136163648@y=135773312@z=14@mm=ZP@ax=136165888@ay=135774592@at=Pal%C3%A1c%20Magnum@ad=Koldasoft,%20s.r.o\" title=\"Najít adresu na Mapy.cz\">mapa</a>)</p>
             <p>530&nbsp;02 Pardubice &ndash; Zelené Předměstí</p>
-            <p></p>
+            <p>Budova MAGNUM, 6. patro</p>
             <h3>Další kontaktní informace</h3>
-            <p>Jednatel společnosti: <strong>Jiří Kolařík</strong></p>
-            <p>Telefonní kontakt: <strong>724&nbsp;059&nbsp;012</strong>&nbsp;(Vodafone) a <strong>775&nbsp;770&nbsp;178</strong>&nbsp;(Vodafone)</p>
-            <p>Elektronická pošta: <a href=\"mailto:info@koldasoft.cz\" title=\"Poslat E-mail\"><strong>info@koldasoft.cz</strong></a></p>
+            <p>Telefon: <strong>775&nbsp;77&nbsp;15&nbsp;00</strong>&nbsp;(".mylink("nas-tym", "Jiří Kolařík", "kolda", "Jiří Kolařík").", ředitel) a <strong>775&nbsp;77&nbsp;15&nbsp;10</strong>&nbsp;(".mylink("nas-tym", "Lucie Brandýská", "lucka", "Lucie Brandýská").", asistentka)</p>
+            <p>E-mail: <a href=\"mailto:info@koldasoft.cz\" title=\"Poslat E-mail\"><strong>info@koldasoft.cz</strong></a></p>
             <p>Webové stránky: <a href=\"http://www.koldasoft.cz/\" title=\"Přejít na webové stránky společnosti Koldasoft, s.r.o.\"><strong>www.koldasoft.cz</strong></a></p>
-            <p>Neváhejte kontakovat kohokoliv z našeho týmu: ".mylink("nas-tym", "seznam členů týmu").".</p>
+            <p>Neváhejte také kontaktovat kohokoliv z&nbsp;".mylink("nas-tym", "našeho týmu").".</p>
+            <h3>Kontaktní formulář</h3>
           </div>  
           <div class=\"text_box\">
           </div>".nl);
-          
-/*
-            <h2 id=\"kontaktni-formular\">Kontaktní formulář</h2>
-            <p>Máte nějaký dotaz, nápad, připomínku nebo si chcete domluvit osobní schůzku? Můžete využít tento jednoduchý formulář. Rádi Vám zodpovíme všechny Vaše dotazy.</p>
-            <form action=\"index.php\" method=\"post\">
-              <p><input type=\"hidden\" name=\"send_mail\" value=\"1\" /></p> 
-              <table>
-                <tr>
-                  <td>Jméno: </td><td><input class=\"sede\" type=\"text\" name=\"form[jmeno]\" size=\"20\" value=\"\" /></td>
-                </tr>
-                <tr>
-                  <td>E-mail: </td><td><input class=\"sede\" type=\"text\" name=\"form[mail]\" size=\"20\" value=\"\" /></td>              
-                </tr>
-                <tr>
-                  <td>Text: </td><td><textarea class=\"sede\" rows=\"4\" cols=\"50\" name=\"text\"></textarea></td>              
-                </tr>
-              </table>              
-              <p><input class=\"button\" type=\"submit\" alt=\"Odeslat\" /></p>
-            </form>        
-*/          
+                  
 
 ?>
