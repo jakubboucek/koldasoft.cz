3c26d7ea8c199bfe29122f86d814b108fd4eb816
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-12 18:04:16 +0000

    Upravený obsah stránky "Jak by měla vypadat kvalitní webová stránka".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@111 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/informace/pozadavky-kvalitni-stranky.php
+++ source/pages/informace/pozadavky-kvalitni-stranky.php
@@ -2,43 +2,63 @@
 
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
-            <p>Mít kvalitní webové stránky je základem úspěchu! Zajímá Vás, co si představit pod pojmem kvalitní webová stránka? Jak taková stránka vypadá a jak se pozná?</p>
-          </div>  
-          <div class=\"text_box\">
-            <h2>Požadavky</h2>
-            <p>Kvalitní webová stránka by měla splňovat několik základních požadavků:</p>
-            <ul class=\"link_farm\">  
-              <li>".mylink("pozadavky-kvalitni-stranky", "Hodnotný obsah", "hodnotny-obsah", "Hodnotný obsah")."</li>
-              <li>".mylink("pozadavky-kvalitni-stranky", "Pěkný grafický návrh (design)", "design", "Pěkný grafický návrh (design)")."</li>
+
+
+          <div class=\"float_box info\">
+            <p class=\"title\"><strong>Kvalitní webová stránka</strong></p>
+            <ul class=\"link_farm\">
               <li>".mylink("pozadavky-kvalitni-stranky", "Validnost, moderní technologie", "validnost", "Validnost, moderní technologie")."</li>
+              <li>".mylink("pozadavky-kvalitni-stranky", "Pěkný grafický návrh (design)", "design", "Pěkný grafický návrh (design)")."</li>
+              <li>".mylink("pozadavky-kvalitni-stranky", "Hodnotný obsah", "hodnotny-obsah", "Hodnotný obsah")."</li>
               <li>".mylink("pozadavky-kvalitni-stranky", "Přístupnost a použitelnost", "pristupnost-pouzitelnost", "Přístupnost a použitelnost")."</li>
               <li>".mylink("pozadavky-kvalitni-stranky", "Podpora různých prohlížečů", "podpora-prohlizecu", "Podpora různých prohlížečů")."</li>
               <li>".mylink("pozadavky-kvalitni-stranky", "Funkčnost bez stylů, obrázků a skriptů", "funkcnost-bez-doplnku", "Funkčnost bez stylů, obrázků a skriptů")."</li>
               <li>".mylink("pozadavky-kvalitni-stranky", "Rychlé načítání", "rychle-nacitani", "Rychlé načítání")."</li>
               <li>".mylink("pozadavky-kvalitni-stranky", "Tisknutelná verze", "tisknutelna-verze", "Tisknutelná verze")."</li>
             </ul>  
-            <h2 id=\"hodnotny-obsah\">Hodnotný obsah</h2>
-            <p>Web, který nenabídne <strong>dostatek informací</strong> ve vhodné míře a kvalitě, ztrácí smysl. Všechny informace by měly být srozumitelné a věrohodné. Webová stránka by měla obsahovat zejména kontakt na firmu (včetně všech náležitostí), nabídku služeb či produktů a ceník.</p>
-            <p>Návštěvníci hledají na internetu převážně informace, tak proč jim je nenabídnout?</p>
-            <h2 id=\"design\">Pěkný grafický návrh (design)</h2>
-            <p>Zajímavý, pěkný a přehledný design je základem pro každou webovou stránku a doplňuje tak její hodnotný obsah. Design <strong>zapůsobí na návštěvníka</strong> prvním dojmem již při vstupu na stránku. Design celé stránky musí být shodný s firemním grafickým stylem (corporate design) a tvořit tak spolu s ostatními propagačními materiály jednotný celek.</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
+            <p>Mít kvalitní webové stránky je základem úspěchu! Zajímá Vás, co si představit pod pojmem kvalitní webová stránka? Jak taková stránka vypadá a jak se pozná?</p>
+          </div>  
+          <div class=\"text_box\">
+
+
             <h2 id=\"validnost\">Validnost, moderní technologie</h2>
             <p>Webové stránky by měly splňovat požadavky a doporučení konsorcia <acronym>W3C</acronym>. Zejména se jedná o <acronym>validní</acronym> a <acronym>sémanticky</acronym> správný kód. Při tvorbě by měly být použité moderní a osvědčené technologie tak, aby výsledná webová stránka <strong>odpovídala pravidlům</strong> v oblasti prezentace na internetu.</p>
             <p>Kontrolu, zda je kód dané webové stránky validní, lze <strong>snadno ověřit</strong> pomocí online validátoru na adrese <a href=\"http://validator.w3.org/\" title=\"Online validátor\">http://validator.w3.org/</a>.</p>
+
+            <h2 id=\"design\">Pěkný grafický návrh (design)</h2>
+            <p>Zajímavý, pěkný a přehledný design je základem pro každou webovou stránku a doplňuje tak její hodnotný obsah. Design <strong>zapůsobí na návštěvníka</strong> prvním dojmem již při vstupu na stránku. Design celé stránky musí být shodný s&nbsp;firemním grafickým stylem (corporate design) a tvořit tak spolu s ostatními propagačními materiály jednotný celek.</p>
+
+
+          <div class=\"float_box tip\">
+            <p class=\"title\"><strong>Kvalitní obsah především</strong></p>
+            <p>Návštěvníci hledají na internetu převážně informace, tak proč jim je nenabídnout?</p>
+            <p>Kvalitní obsah si získá návštěvníky a přiměje je k&nbsp;opětovnému návratu!</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
+            <h2 id=\"hodnotny-obsah\">Hodnotný obsah</h2>
+            <p>Web, který nenabídne <strong>dostatek informací</strong> ve vhodné míře a kvalitě, ztrácí smysl. Všechny informace by měly být srozumitelné a věrohodné. Webová stránka by měla obsahovat zejména kontakt na firmu (včetně všech náležitostí), nabídku služeb či produktů a ceník.</p>
+
+
             <h2 id=\"pristupnost-pouzitelnost\">Přístupnost a použitelnost</h2>
-            <p>Kvalitní webové stránky by měly být ".mylink("pouzitelnost-pristupnost", "použitelné a přístupné").". To znamená, že budou bez omezení k dispozici všem návštěvníkům, potenciálním zákazníkům. Nezáleží tedy na tom, jaký počítač (zařízení) uživatel používá, jestli má nějaká omezení, vždy <strong>dostane shodné a úplné informace</strong>. Návštěvník by měl Vaší stránce plně rozumět, orientovat se v navigaci a celkově se na Vašem webu cítit dobře.</p>
+            <p>Kvalitní webové stránky by měly být ".mylink("pouzitelnost-pristupnost", "použitelné a přístupné").". To znamená, že budou bez omezení k&nbsp;dispozici všem návštěvníkům, potenciálním zákazníkům. Nezáleží tedy na tom, jaký počítač (zařízení) uživatel používá, jestli má nějaká omezení, vždy <strong>dostane shodné a úplné informace</strong>. Návštěvník by měl Vaší stránce plně rozumět, orientovat se v&nbsp;navigaci a celkově se na Vašem webu cítit dobře.</p>
             <h2 id=\"podpora-prohlizecu\">Podpora různých prohlížečů</h2>
-            <p>I když se podpora prohlížečů řadí mezi prvky ".mylink("pouzitelnost-pristupnost", "použitelnosti").", bylo by vhodné se nad tímto tématem pozastavit. V dnešní době mnohem <strong>více uživatelů používá alternativní <acronym>prohlížeče</acronym></strong> k prohlížení webových stránek. Za alternativní považujeme všechny ostatní prohlížeče kromě Internet Exploreru. Mezi nejznámější pak patří například Mozilla Firefox nebo Opera. Alternativním prohlížečem může být i prohlížeč textový (např. Lynx). I přes zavedené standardy se webové stránky v různých prohlížečích zobrazují různě. Nejčastěji používaný prohlížeč Internet Explorer mnohé standardy nepodporuje vůbec a některé chybně. Webdesigner je tak velmi omezen a je mnohdy velkým oříškem naprogramovat webovou stránku tak, aby byla korektně zobrazovaná ve všech prohlížečích.</p>
+            <p>I když se podpora prohlížečů řadí mezi prvky ".mylink("pouzitelnost-pristupnost", "použitelnosti").", bylo by vhodné se nad tímto tématem pozastavit. V&nbsp;dnešní době mnohem <strong>více uživatelů používá alternativní <acronym>prohlížeče</acronym></strong> k&nbsp;prohlížení webových stránek. Za alternativní považujeme všechny ostatní prohlížeče kromě Internet Exploreru. Mezi nejznámější pak patří například Mozilla Firefox nebo Opera. Alternativním prohlížečem může být i prohlížeč textový (např. Lynx). I přes zavedené standardy se webové stránky v&nbsp;různých prohlížečích zobrazují různě. Nejčastěji používaný prohlížeč Internet Explorer mnohé standardy nepodporuje vůbec a některé chybně. Webdesigner je tak velmi omezen a je mnohdy velkým oříškem naprogramovat webovou stránku tak, aby byla korektně zobrazovaná ve všech prohlížečích.</p>
             <h2 id=\"funkcnost-bez-doplnku\">Funkčnost bez stylů, obrázků a skriptů</h2>
             <p>Dnešní webové stránky obsahují kromě vlastního textu i mnoho doplňků: styly, obrázky, skripty a další multimédia. Kvalitní webové stránky musí být <strong>použitelné i bez těchto doplňků</strong>.</p>
             <p><strong>Styly</strong> určují, jak má být stránka zobrazena. Styly nemusí být vždy žádoucí (např. <acronym>hlasová čtečka</acronym>) a stránka tak musí být bez problému použitelná i bez stylů.</p>
-            <p><strong>Obrázky</strong> nejčastěji slouží jako doplňující grafika, mnohdy však zastupují i tak důležité prvky jako je menu či nadpis. Pokud se obrázek z nějakého důvodu nenačte (uživatel může mít vypnuté načítání obrázků, obrázek je poškozen, textový prohlížeč), musí být uveden alternativní text, který je zobrazen namísto obrázku.</p>
+            <p><strong>Obrázky</strong> nejčastěji slouží jako doplňující grafika, mnohdy však zastupují i tak důležité prvky jako je menu či nadpis. Pokud se obrázek z&nbsp;nějakého důvodu nenačte (uživatel může mít vypnuté načítání obrázků, obrázek je poškozen, textový prohlížeč), musí být uveden alternativní text, který je zobrazen namísto obrázku.</p>
             <p>Velmi oblíbenou součástí webových stránek jsou <strong>flashové animace</strong>. Pokud je použita technologie <acronym>flash</acronym>, vždy by měla být k dispozici její textová nebo grafická alternativa. Další nevýhodou je, že pro zobrazení této animace musí být nainstalovaný příslušný plugin – zásuvný model pro flash animace. Ne všechny počítače tímto pluginem disponují a někdy jsou flash animace dokonce zakázány.</p>
-            <p>Mezi <strong>skripty</strong> se řadí například JavaScript. Skripty by měly uživateli pobyt na webových stránkách zpříjemnit. Používají se především pro kontrolu formuláře před odesláním, počítání zadaných znaků, nebo pro práci s časem. Skriptování bývá mnohdy z bezpečnostních důvodů zakázáno a proto by stránky měly být použitelné i bez skriptů.</p>
+            <p>Mezi <strong>skripty</strong> se řadí například JavaScript. Skripty by měly uživateli pobyt na webových stránkách zpříjemnit. Používají se především pro kontrolu formuláře před odesláním, počítání zadaných znaků, nebo pro práci s časem. Skriptování bývá mnohdy z&nbsp;bezpečnostních důvodů zakázáno a proto by stránky měly být použitelné i bez skriptů.</p>
             <h2 id=\"rychle-nacitani\">Rychlé načítání</h2>
-            <p>Webová stránka je velmi specifickým médiem, a tak se k ní také musí přistupovat. Doba, za kterou se požadovaná webová stránka otevře a zobrazí, je velmi důležitá. <strong>Nikdo nechce čekat dlouho</strong> jen proto, aby viděl nezajímavé video či obrovskou fotografii. Stránky, zejména použitá grafika, musí být optimalizována tak, aby načítání bylo co možná nejrychlejší. Doba, za kterou se návštěvníkovi stránka zobrazí, záleží na rychlosti připojení k internetu. Nezapomínejme však na to, že i dnes je stále spousta uživatelů připojená přes telefonní linku (tzv. vytáčené připojení) či mobilní telefon a otevření velké, graficky náročné, stránky může být záležitostí i několika minut. Kvalitní webové stránky mají veškerý obsah optimalizovaný a doba otevření je tak pro většinu uživatelů akceptovatelná.</p>
+            <p>Webová stránka je velmi specifickým médiem, a tak se k&nbsp;ní také musí přistupovat. Doba, za kterou se požadovaná webová stránka otevře a zobrazí, je velmi důležitá. <strong>Nikdo nechce čekat dlouho</strong> jen proto, aby viděl nezajímavé video či obrovskou fotografii. Stránky, zejména použitá grafika, musí být optimalizována tak, aby načítání bylo co možná nejrychlejší. Doba, za kterou se návštěvníkovi stránka zobrazí, záleží na rychlosti připojení k&nbsp;internetu. Nezapomínejme však na to, že i dnes je stále spousta uživatelů připojená přes telefonní linku (tzv. vytáčené připojení) či moderněji přes mobilní telefon a otevření velké, graficky náročné, stránky může být záležitostí i několika minut. Kvalitní webové stránky mají veškerý obsah optimalizovaný a doba otevření je tak pro většinu uživatelů akceptovatelná.</p>
             <h2 id=\"tisknutelna-verze\">Tisknutelná verze</h2>
-            <p>I v dnešní digitální době je stále mnoho lidí, kteří upřednostňují papír a tužku. Uživatelé si informace z internetu rádi vytisknou a přečtou raději v &bdquo;papírové formě&ldquo;, než na obrazovce monitoru. Kvalitní webové stránky <strong>umožňují veškerý obsah vytisknout</strong> ve vhodné formě nebo odkazují na tisknutelnou verzi, popř. na stránku ve formátu PDF.</p>
+            <p>I v&nbsp;dnešní digitální době je stále mnoho lidí, kteří upřednostňují papír a tužku. Uživatelé si informace z&nbsp;internetu rádi vytisknou a přečtou raději v&nbsp;&bdquo;papírové formě&ldquo;, než na obrazovce monitoru. Kvalitní webové stránky <strong>umožňují veškerý obsah vytisknout</strong> ve vhodné formě nebo odkazují na tisknutelnou verzi, popř. na stránku ve formátu PDF.</p>
           </div>".nl);
 
 ?>
