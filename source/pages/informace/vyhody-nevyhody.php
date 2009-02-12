80ec501cdbf8a5f1210390020698dcdf10e87f94
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-12 21:40:09 +0000

    Upravený obsah stránky "Výhody a nevýhody webových stránek".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@114 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/informace/vyhody-nevyhody.php
+++ source/pages/informace/vyhody-nevyhody.php
@@ -7,7 +7,7 @@
           </div>  
           <div class=\"text_box\">
             <h2 id=\"vyhody\">Výhody webových stránek</h2>
-            <p>Výhodou je jistě rozšířenost a prostor pro prezentaci své práce bez nutnosti návštěvy obchodu či kanceláře nějaké organizace, popřípadě čekání ve frontě, pro nějakou drobnou informaci na úřadech. Za normálních okolností jsou stránky přístupné 24 hodin denně, a tak se zájemce o služby nemusí starat o provozní dobu firmy.</p>
+            <p>Výhodou je jistě rozšířenost a prostor pro prezentaci své práce bez nutnosti návštěvy obchodu či kanceláře nějaké organizace, popřípadě čekání ve frontě, pro nějakou drobnou informaci na úřadech. Za normálních okolností jsou stránky přístupné 24&nbsp;hodin denně, a tak se zájemce o služby nemusí starat o provozní dobu firmy.</p>
             <p>Nejdůležitější výhody:</p>
             <ul>
               <li><strong>Celosvětová dostupnost</strong> - na webové stránky je možné podívat se kdykoliv a odkudkoliv.</li>
@@ -15,17 +15,17 @@
               <li><strong>Interaktivnost</strong> - webové stránky reagují přesně na Vaše požadavky a podněty. Vy si zde volíte, jaký obsah se bude zobrazovat, jaké zprávy Vás zajímají, jakou barvu máte rádi, &hellip;</li>
               <li><strong>Snadné a rychlé aktualizace</strong> umožňují udržovat webové stránky stále aktuální.</li>
               <li><strong>Snadné ovládání</strong> potěší i slabší uživatele.</li>
-              <li><strong>Ryhlá komunikace</strong> probíhá především prostřednictvím elektronické pošty, E-mailu. Odeslání a přijetí E-mailu druhou stranou je otázkou vteřin a odeslat lze spolu s textem téměř cokoliv (dokument, obrázek).</li>
+              <li><strong>Ryhlá komunikace</strong> probíhá především prostřednictvím elektronické pošty, E-mailu. Odeslání a přijetí E-mailu druhou stranou je otázkou vteřin a odeslat lze spolu s&nbsp;textem téměř cokoliv (dokument, obrázek).</li>
               <li><strong>Snadná navigace</strong> pomocí odkazů. Procházením webu lze získat požadované informace snadno a rychle.</li>
-              <li><strong>Přímý prodej</strong> přes internet, tzv. E-shop, umožňuje nakupovat snadno a rychle z pohodlí Vašeho domova.</li>
+              <li><strong>Přímý prodej</strong> přes internet, tzv. E-shop, umožňuje nakupovat snadno a rychle z&nbsp;pohodlí Vašeho domova.</li>
               <li><strong>Nízká cena</strong> ".mylink("cenik", "vytvoření")." stránek a jejich ".mylink("naklady-na-provoz", "provozu").".</li>
             </ul>  
             <h2 id=\"nevyhody\">Nevýhody webových stránek</h2>
-            <p>Mezi největší nevýhody patří skutečnost, že stále <strong>nemají přístup</strong> na internet všichni. Nejhůře jsou na tom malé vesnice a obce. Někteří lidé, zejména starší generace, počítače a internet neuznávají, a nebo s ním, bohužel, <strong>neumějí zacházet</strong>. Tito lidé pak Vaše webové stránky nikdy neuvidí a nelze se tedy spoléhat pouze na tento druh propagace.</p>
+            <p>Mezi největší nevýhody patří skutečnost, že stále <strong>nemají přístup</strong> na internet všichni. Nejhůře jsou na tom malé vesnice a obce. Někteří lidé, zejména starší generace, počítače a internet neuznávají, a nebo s&nbsp;ním, bohužel, <strong>neumějí zacházet</strong>. Tito lidé pak Vaše webové stránky nikdy neuvidí a nelze se tedy spoléhat pouze na tento druh propagace.</p>
             <p>Velkou nevýhodou je fakt, že nikdo neví, <strong>jak se daná stránka zobrazí</strong> návštěvníkovi. Záleží zde na spoustě faktorů, zejména na monitoru (velikosti, barvách), na nainstalovaných doplňcích (podpora JavaScriptu, Flashe). Uživatelé dále mohou mít vypnuté zobrazování obrázků, nebo stránka může být různě reprezentována (textový prohlížeč, hlasová čtečka). ".mylink("pozadavky-kvalitni-stranky", "Kvalitní webové stránky")." Vám zajistí, že se všem návštěvníkům dostane shodných informací. Vzhled ale nemůže být nikdy stoprocentní, a tak nechtějte tedy po webdesignerovi přesnou podobu webových stránek, chtěli byste nemožné.</p>
             <p>Pokud by Vaše webové stránky nebyly ".mylink("pozadavky-kvalitni-stranky", "kvalitní").", ".mylink("pouzitelnost-pristupnost", "přístupné a použitelné").", mohlo by se snadno stát, že návštěvníky (popř. obchodní partnery) spíše podráždíte nebo úplně odradíte.</p>
             <p>Vaše webové  <strong>".mylink("nasledna-sprava", "stránky je nutné udržovat")."</strong>, tedy aktualizovat, a po nějaké době obnovovat tak, aby stránky nezestárnuly.</p>
-            <p>Na internetu je <strong>velká konkurence</strong> v každém oboru. Nesoupeříte tak pouze s jednou firmou z Vašeho okolí.</p>
+            <p>Na internetu je <strong>velká konkurence</strong> v každém oboru. Nesoupeříte tak pouze s jednou firmou z&nbsp;Vašeho okolí.</p>
           </div>".nl);
 
 ?>
