c2880e1fab77a215e71dc7122874d5e3bea507e3
lucka <lucka@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-12 13:41:58 +0000

    nove vizitky, priprava boxiku pro cleny tymu
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@94 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/o-nas/nas-tym.php
+++ source/pages/o-nas/nas-tym.php
@@ -1,10 +1,9 @@
 <?php  //Nas tym
 
   echo("          <div class=\"text_box\">
-
-            <img style=\"float: right;\" src=\"".$cesta."pictures/img.png\" alt=\"PROMO foto\" width=\"400\" height=\"250\" />
             
             <h1>$page_name</h1>
+            <img src=\"".$cesta."pictures/koldasoft_team_photo_01.jpg\" height=\"223\" width=\"630\" alt=\"\" />
             <p>Máme rádi, když víme, s&nbsp;kým jednáme.</p>
             <p>Jsme tým zkušených odborníků s&nbsp;nadšením pro věc. Jsme mladí, ambiciózní a víme, co chceme.</p>
             <p>Nabízíme Vám seznam členů našeho týmu a potřebné kontaktní údaje. </p>
@@ -15,61 +14,62 @@
           <div class=\"cleaner\"></div>
 
           <div class=\"text_box\">
-            <div>
-              <h2 id=\"kolda\">Jiří Kolařík</h2>
-              <img style=\"float: left; margin-right: 10px;\" src=\"".$cesta."pictures/img.png\" alt=\"pasove foto\" width=\"110\" height=\"140\" />
-              <div class=\"float_box\">Lorem ipsum dolor sit amet consectetuer vel justo non ac id. Semper wisi tortor et adipiscing Pellentesque lobortis velit Pellentesque dui molestie.<div class=\"bottom\"></div></div>
-              <p class=\"subtitle\">ředitel společnosti</p>
-              <p><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;00</p>
-              <p><strong>E-mail:</strong> <a href=\"mailto:jiri.kolarik@koldasoft.cz\" title=\"\">jiri.kolarik@koldasoft.cz</a></p>
-              <p><strong>ICQ:</strong> 126-976-186</p>
-              <p><strong>Skype:</strong> jiri.kolda</p>
-              <p><a href=\"".$cesta."files/koldasoft_vizitka_kolarik_v1.0.pdf\" title=\"Vizitka Jiří Kolařík (formát PDF, 33 kB)\">Vizitka</a></p>
+            <div class=\"member\">
+              <h2 id=\"kolda\">Jiří Kolařík : <em>ředitel společnosti</em></h2>
+              <img src=\"".$cesta."pictures/portrait_kolda.jpg\" alt=\"\" width=\"115\" height=\"139\" />
+              <ul>
+                <li><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;00</li>
+                <li><strong>E-mail:</strong> <a href=\"mailto:jiri.kolarik@koldasoft.cz\" title=\"\">jiri.kolarik@koldasoft.cz</a></li>
+                <li><strong>ICQ:</strong> 126-976-186</li>
+                <li><strong>Skype:</strong> jiri.kolda</li>
+                <li><a href=\"".$cesta."files/koldasoft_vizitka_kolarik_v2.1.pdf\" title=\"Vizitka Jiří Kolařík (formát PDF, 32 kB)\">Vizitka</a></li>
+              </ul>
             </div>
             <div class=\"cleaner\"></div>
-            <div>
-              <h2 id=\"lucka\">Lucie Brandýská</h2>
-              <img style=\"float: left; margin-right: 10px;\" src=\"".$cesta."pictures/img.png\" alt=\"pasove foto\" width=\"110\" height=\"140\" />
-              <div class=\"float_box\">Lorem ipsum dolor sit amet consectetuer vel justo non ac id. Semper wisi tortor et adipiscing Pellentesque lobortis velit Pellentesque dui molestie.<div class=\"bottom\"></div></div>
-              <p class=\"subtitle\">asistentka ředitele</p>
-              <p><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;10</p>
-              <p><strong>E-mail:</strong> <a href=\"mailto:lucie.brandyska@koldasoft.cz\" title=\"\">lucie.brandyska@koldasoft.cz</a></p>
-              <p><strong>ICQ:</strong> 212-188-049</p>
+            <div class=\"member\">
+              <h2 id=\"lucka\">Lucie Brandýská : <em>asistentka ředitele</em></h2>
+              <img src=\"".$cesta."pictures/img.png\" alt=\"pasove foto\" width=\"115\" height=\"139\" />
+              <ul>
+                <li><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;10</li>
+                <li><strong>E-mail:</strong> <a href=\"mailto:lucie.brandyska@koldasoft.cz\" title=\"\">lucie.brandyska@koldasoft.cz</a></li>
+                <li><strong>ICQ:</strong> 212-188-049</li>
+                <li><a href=\"".$cesta."files/koldasoft_vizitka_brandyska_v2.1.pdf\" title=\"Vizitka Radek Věchet (formát PDF, 32 kB)\">Vizitka</a></li>
+              </ul>
             </div>
             <div class=\"cleaner\"></div>
-            <div>
-              <h2 id=\"morty\">Radek Věchet</h2>
-              <img style=\"float: left; margin-right: 10px;\" src=\"".$cesta."pictures/img.png\" alt=\"pasove foto\" width=\"110\" height=\"140\" />
-              <div class=\"float_box\">Lorem ipsum dolor sit amet consectetuer vel justo non ac id. Semper wisi tortor et adipiscing Pellentesque lobortis velit Pellentesque dui molestie.<div class=\"bottom\"></div></div>
-              <p class=\"subtitle\">hlavní grafik</p>
-              <p><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;20</p>
-              <p><strong>E-mail:</strong> <a href=\"mailto:radek.vechet@koldasoft.cz\" title=\"\">radek.vechet@koldasoft.cz</a></p>
-              <p><strong>ICQ:</strong> 315-739-033</p>
-              <p><strong>Skype:</strong> xxx_radek_xxx</p>
-              <p><a href=\"".$cesta."files/koldasoft_vizitka_vechet_v1.0.pdf\" title=\"Vizitka Radek Věchet (formát PDF, 33 kB)\">Vizitka</a></p>
+            <div class=\"member\">
+              <h2 id=\"morty\">Radek Věchet : <em>hlavní grafik</em></h2>
+              <img src=\"".$cesta."pictures/img.png\" alt=\"\" width=\"115\" height=\"139\" />
+              <ul>
+              <li><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;20</li>
+              <li><strong>E-mail:</strong> <a href=\"mailto:radek.vechet@koldasoft.cz\" title=\"\">radek.vechet@koldasoft.cz</a></li>
+              <li><strong>ICQ:</strong> 315-739-033</li>
+              <li><strong>Skype:</strong> xxx_radek_xxx</li>
+              <li><a href=\"".$cesta."files/koldasoft_vizitka_vechet_v2.1.pdf\" title=\"Vizitka Radek Věchet (formát PDF, 32 kB)\">Vizitka</a></li>
+              </ul>
             </div>
             <div class=\"cleaner\"></div>
-            <div>
-              <h2 id=\"bukaj\">Jakub Bouček</h2>
-              <img style=\"float: left; margin-right: 10px;\" src=\"".$cesta."pictures/img.png\" alt=\"pasove foto\" width=\"110\" height=\"140\" />
-              <div class=\"float_box\">Lorem ipsum dolor sit amet consectetuer vel justo non ac id. Semper wisi tortor et adipiscing Pellentesque lobortis velit Pellentesque dui molestie.<div class=\"bottom\"></div></div>
-              <p class=\"subtitle\">programátor</p>
-              <p><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;30</p>
-              <p><strong>E-mail:</strong> <a href=\"mailto:jakub.boucek@koldasoft.cz\" title=\"\">jakub.boucek@koldasoft.cz</a></p>
-              <p><strong>ICQ:</strong> 149-366-049</p>
-              <p><strong>Skype:</strong> jakub.boucek.info</p>
-              <p><a href=\"".$cesta."files/koldasoft_vizitka_boucek_v1.0.pdf\" title=\"Vizitka Jakub Bouček (formát PDF, 33 kB)\">Vizitka</a></p>
+            <div class=\"member\">
+              <h2 id=\"bukaj\">Jakub Bouček : <em>hlavní programátor</em></h2>
+              <img src=\"".$cesta."pictures/portrait_bukaj.jpg\" alt=\"pasove foto\" width=\"115\" height=\"139\" />
+              <ul>
+                <li><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;30</li>
+                <li><strong>E-mail:</strong> <a href=\"mailto:jakub.boucek@koldasoft.cz\" title=\"\">jakub.boucek@koldasoft.cz</a></li>
+                <li><strong>ICQ:</strong> 149-366-049</li>
+                <li><strong>Skype:</strong> jakub.boucek.info</li>
+                <li><a href=\"".$cesta."files/koldasoft_vizitka_boucek_v2.1.pdf\" title=\"Vizitka Jakub Bouček (formát PDF, 32 kB)\">Vizitka</a></li>
+              </ul>
             </div>
             <div class=\"cleaner\"></div>
-            <div>
-              <h2 id=\"harem_master\">Jiří Drahorád</h2>
-              <img style=\"float: left; margin-right: 10px;\" src=\"".$cesta."pictures/img.png\" alt=\"pasove foto\" width=\"110\" height=\"140\" />
-              <div class=\"float_box\">Lorem ipsum dolor sit amet consectetuer vel justo non ac id. Semper wisi tortor et adipiscing Pellentesque lobortis velit Pellentesque dui molestie.<div class=\"bottom\"></div></div>
-              <p class=\"subtitle\">designer</p>
-              <p><strong>Mobil:</strong> (+420) 720&nbsp;24&nbsp;92&nbsp;92</p>
-              <p><strong>E-mail:</strong> <a href=\"mailto:jiri.drahorad@koldasoft.cz\" title=\"\">jiri.drahorad@koldasoft.cz</a></p>
-              <p><strong>ICQ:</strong> 233-445-142</p>
-              <p><a href=\"".$cesta."files/koldasoft_vizitka_drahorad_v1.0.pdf\" title=\"Vizitka Jiří Drahorád (formát PDF, 33 kB)\">Vizitka</a></p>
+            <div class=\"member\">
+              <h2 id=\"harem_master\">Jiří Drahorád : <em>designer</em></h2>
+              <img src=\"".$cesta."pictures/img.png\" alt=\"pasove foto\" width=\"115\" height=\"139\" />
+              <ul>
+                <li><strong>Mobil:</strong> (+420) 720&nbsp;24&nbsp;92&nbsp;92</li>
+                <li><strong>E-mail:</strong> <a href=\"mailto:jiri.drahorad@koldasoft.cz\" title=\"\">jiri.drahorad@koldasoft.cz</a></li>
+                <li><strong>ICQ:</strong> 233-445-142</li>
+                <li><a href=\"".$cesta."files/koldasoft_vizitka_drahorad_v2.1.pdf\" title=\"Vizitka Jiří Drahorád (formát PDF, 32 kB)\">Vizitka</a></li>
+              </ul>
             </div>
           </div>".nl);
 
