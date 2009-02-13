6134487dbc44785c71d0fe2ba70bc35f95353b23
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-13 08:05:24 +0000

    Upravený obsah stránky "Registrace do katalogů".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@136 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/nabidka-sluzeb/tvorba/registrace-do-katalogu.php
+++ source/pages/nabidka-sluzeb/tvorba/registrace-do-katalogu.php
@@ -2,14 +2,40 @@
 
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
-            <p>Katalogy jsou jakousi moderní <strong>obdobou zlatých stránek</strong> v telefonním seznamu. Katalogy jsou určeny k přehlednému utřídění webových stránek podle kategorií. Vybráním kategorie, popř. okresu či jiných specifik, se postupně dostaneme až na seznam hledaných firem.</p>
-            <p>Mezi nejznámější české katalogy patří <a href=\"http://katalog.seznam.cz/\">Seznam</a>, <a href=\"http://katalog.centrum.cz/\">Centrum</a> a <a href=\"http://katalog.atlas.cz/\">Atlas</a>. Ze zahraničních katalogů jsou nejznámější <a href=\"http://www.yahoo.com/\">Yahoo!</a> a <a href=\"http://www.dmoz.org/World/Česky/\">ODP</a>. Existují také katalogy s daným zaměřením.</p>
-            <p>Pokud se Vaše firma nenachází v katalozích, vítězí konkurence.</p>
+
+
+          <div class=\"float_box tip\">
+            <p class=\"title\"><strong>Nejznámější katalogy</strong></p>
+            <ul>
+              <li><a href=\"http://katalog.seznam.cz/\">Seznam</a> - nejrozsáhlejší český katalog</li>
+              <li><a href=\"http://www.najisto.cz/\">Najisto</a> - nový portál (Centrum + Atlas)</li>
+              <li><a href=\"http://www.yahoo.com/\">Yahoo!</a> - zahraniční portál</li>
+              <li><a href=\"http://www.dmoz.org/World/Česky/\">ODP</a> - zahraniční portál</li>
+            </ul>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
+            <p>Katalogy jsou jakousi moderní <strong>obdobou zlatých stránek</strong> v&nbsp;telefonním seznamu. Katalogy jsou určeny k&nbsp;přehlednému utřídění webových stránek podle kategorií. Vybráním kategorie, popř. okresu či jiných specifik, se postupně dostaneme až na seznam hledaných firem.</p>
+            <p>Pokud se Vaše firma nenachází v&nbsp;katalozích, vítězí konkurence.</p>
           </div>  
           <div class=\"text_box\">
             <h2 id=\"rozdil-katalog-vyhledavac\">Rozdíl mezi katalogem a vyhledávačem</h2>
             <p><strong>Katalog</strong> je ručně zařazovaný seznam webových stránek podle zaměření, adresy, okresu, &hellip; Do katalogu je nutné přidat odkaz na Vaše webové stránky ručně (zažádat o vložení).</p>
-            <p><strong>Vyhledávač</strong> (mezi nejznámější patří <a href=\"http://google.cz/\">Google</a>, <a href=\"http://jyxo.cz/\">Jyxo</a>, <a href=\"http://morfeo.cz/\">Morfeo</a>) používá tzv. roboty (boty, crawlery, spidery), což jsou programy, které sami procházejí internetem, prohlížejí webové stránky a tzv. je indexují (uloží si je do své databáze). Do vyhledávače se Vaše webová stránka nezadává, vyhledávač si jí najde sám. Pokud na Vaše stránky tento robot zavítá, je potřeba mu nabídnout kvalitní a relevantní informace. K tomu potřebujete ".mylink("pozadavky-kvalitni-stranky", "kvalitní webové stránky").", ".mylink("optimalizace-pro-vyhledavace", "optimalizované pro vyhledávače (SEO)").".</p>
+
+
+          <div class=\"float_box tip\">
+            <p class=\"title\"><strong>Nejznámější vyhledávače</strong></p>
+            <ul>
+              <li><a href=\"http://google.cz/\">Google</a> - nejlepší celosvětový vyhledávač</li>
+              <li><a href=\"http://seznam.cz/\">Seznam</a> - kvalitní český vyhledávač</li>
+              <li><a href=\"http://jyxo.cz/\">Jyxo</a> - ryze český projekt</li>
+            </ul>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
+            <p><strong>Vyhledávač</strong> používá tzv. roboty (boty, crawlery, spidery), což jsou programy, které sami procházejí internetem, prohlížejí webové stránky a tzv. je indexují (uloží si je do své databáze). Do vyhledávače se Vaše webová stránka nezadává, vyhledávač si jí najde sám. Pokud na Vaše stránky tento robot zavítá, je potřeba mu nabídnout kvalitní a relevantní informace. K&nbsp;tomu potřebujete ".mylink("pozadavky-kvalitni-stranky", "kvalitní webové stránky").", ".mylink("optimalizace-pro-vyhledavace", "optimalizované pro vyhledávače (SEO)").".</p>
             <h2 id=\"co-vam-nabizime\">Co Vám nabízíme?</h2>
             <p>Provedeme registraci do katalogů za použití vhodných <acronym>klíčových slov</acronym> pro Vaše webové stránky a odkaz správně zatřídíme. Provedeme kontrolu správnosti zařazení. Vaše webové stránky tak budou snadno nalezitelné přes katalogy, získáte tím <acronym>zpětné odkazy</acronym>, důležité zejména pro vyhledávače.</p>
           </div>".nl);
