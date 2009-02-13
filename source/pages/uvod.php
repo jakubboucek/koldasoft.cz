b444e3378fd2783b327b20588ce1cb203591af60
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-13 22:58:59 +0000

    Upravený obsah stránky "Domovská stránka".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@149 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/uvod.php
+++ source/pages/uvod.php
@@ -3,35 +3,50 @@
   echo("          <div class=\"text_box\">
             <h1>Koldasoft</h1>
             <p>Snadná cesta k vlastnímu webu.</p>
-            <div class=\"half left\">
-              <h2>Naše nabídka</h2>
-              <p>Nabízíme komplexní služby, které zahrnují zejména:</p>
-              <ul class=\"link_farm\">
+
+
+          <div class=\"float_box left tip more\">
+            <div class=\"title\"><strong>".mylink("nabidka-sluzeb", "Naše nabídka")."</strong></div>
+            <p>Nabízíme komplexní služby, zejména:</p>
+            <ul>
                 <li>".mylink("tvorba")."</li>
                 <li>".mylink("redesign", "Přepracování webových stránek")."</li>
                 <li>".mylink("graficke-prace")."</li>
-              </ul>          
-              <p class=\"read_next\">".mylink("nabidka-sluzeb", "Kompletní nabídka služeb&hellip;")."</p>          
-            </div>
-            <div class=\"half right\">
-              <h2>Proč si vybrat nás</h2>
-              <p>Několik důvodů, proč je Koldasoft Vaše správná volba:</p>
-              <ul class=\"link_farm\">
-                <li>".mylink("proc-zvolit-koldasoft", "Nabízíme komplexní služby", "komplexni-sluzby", "Nabízíme komplexní služby")."</li>
-                <li>".mylink("proc-zvolit-koldasoft", "Nabízíme zajímavé ceny", "zajimave-ceny", "Nabízíme zajímavé ceny")."</li>  
-                <li>".mylink("proc-zvolit-koldasoft", "Máme chuť a odhodlání", "chut-odhodlani", "Máme chuť a odhodlání")."</li>  
-              </ul>          
-              <p class=\"read_next\">".mylink("proc-zvolit-koldasoft", "Proč zvolit Koldasoft&hellip;")."</p>          
-            </div>
+            </ul>          
+            <div class=\"bottom\"><p class=\"read_next\">".mylink("nabidka-sluzeb", "Kompletní nabídka služeb&hellip;")."</p></div>
+          </div>  
+
+
+          <div class=\"float_box question more\">
+            <div class=\"title\"><strong>".mylink("proc-zvolit-koldasoft", "Proč si vybrat nás?")."</strong></div>
+            <p>Nabízíme komplexní služby, zejména:</p>
+            <ul>
+              <li>".mylink("tvorba")."</li>
+              <li>".mylink("redesign", "Přepracování webových stránek")."</li>
+              <li>".mylink("graficke-prace")."</li>
+            </ul>          
+            <div class=\"bottom\"><p class=\"read_next\">".mylink("proc-zvolit-koldasoft", "Proč zvolit Koldasoft&hellip;")."</p></div>
+          </div>  
+
+
             <div class=\"cleaner\">&nbsp;</div>
           </div>  
           <div class=\"text_box\">
             <h2 id=\"proc-mit-stranky\">".mylink("proc-mit-stranky")."</h2>
             <p>Přemýšlíte, zda je dobré mít vlastní webové stránky? A proč? Nabídneme Vám několik pádných důvodů, proč byste je měli mít právě Vy&hellip; ".mylink("proc-mit-stranky", "čtěte dále")."</p>
-            <h2 id=\"proc-nechat-delat\">".mylink("proc-nechat-delat")."</h2>
-            <p>S vytvořením, umístěním a provozem webových stránek je spojeno mnoho náležitostí. Chcete znát důvody pro přenechání práce odborníkům? ".mylink("proc-nechat-delat", "čtěte dále")."</p>
             <h2 id=\"kvalitni-webova-stranka\">".mylink("pozadavky-kvalitni-stranky")."</h2>
             <p>Mít kvalitní webové stránky je základem úspěchu! Zajímá Vás, co si představit pod pojmem kvalitní webová stránka? Jak vypadá, jak se pozná a jaké jsou její náležitosti? ".mylink("pozadavky-kvalitni-stranky", "čtěte dále")."</p>
+            <h2 id=\"proc-nechat-delat\">".mylink("proc-nechat-delat")."</h2>
+            <p>S&nbsp;vytvořením, umístěním a provozem webových stránek je spojeno mnoho náležitostí. Chcete znát důvody pro přenechání práce odborníkům? ".mylink("proc-nechat-delat", "čtěte dále")."</p>
+
+
+          <div class=\"float_box tip more\">
+            <div class=\"title\"><strong>".mylink("nas-tym", "Náš tým")."</strong></div>
+            <p>Hledáte někoho konkrétního? Nebo Vás jen zajímá, kdo jsme?</p>
+            <div class=\"bottom\"><p class=\"read_next\">".mylink("nas-tym", "Koukněte na náš tým")."</p></div>
+          </div>  
+
+
             <h2>".mylink("o-nas")."</h2>
             <p>Jsme mladý tým odborníků, který se zabývá prezentací na internetu. Naší snahou je zdokonalovat své schopnosti a tím i zvyšovat <strong>kvalitu naší práce</strong>. Chceme se podílet na vývoji, zlepšování a kultivaci prostředí českého i zahraničního internetu. ".mylink("o-nas", "čtěte dále")."</p>
           </div>".nl);
