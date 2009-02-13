a3e12637f82a328fd7316931c44ec60ce52aeba6
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-13 08:26:37 +0000

    Upravený obsah stránky "Optimalizace pro vyhledávače".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@137 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/nabidka-sluzeb/tvorba/optimalizace-pro-vyhledavace.php
+++ source/pages/nabidka-sluzeb/tvorba/optimalizace-pro-vyhledavace.php
@@ -2,8 +2,16 @@
 
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
-            <p>Stránky, které nikdo nenajde, jakoby nebyly. Našim cílem je stránky optimalizovat pomocí metod <acronym>SEO</acronym> tak, aby byly snadno k nalezení <acronym>vyhledávači</acronym> a vyskytovaly se na předních pozicích výsledků vyhledávání. <strong>Tato optimalizace je možná důležitější než vlastní webové stránky.</strong> Lze se velmi snadno zviditelnit a předstihnout tak konkurenci, protože většina návštěvníků (přes 80%), přichází právě přes internetové vyhledávače.</p>
-            <p>Za pomoci tohoto nástroje pro internetový marketing zajistíte Vašim webovým stránkám vyšší pozice ve vyhledávačích, zvýšíte tím počet návštěvníků, potenciálních zákazníků, a tím i výši Vašich zisků.</p>
+            <p>Stránky, které nikdo nenajde, jakoby nebyly. Našim cílem je stránky optimalizovat pomocí metod <acronym>SEO</acronym> tak, aby byly snadno k&nbsp;nalezení <acronym>vyhledávači</acronym> a vyskytovaly se na předních pozicích výsledků vyhledávání. <strong>Tato optimalizace je možná důležitější než vlastní webové stránky.</strong> Lze se velmi snadno zviditelnit a předstihnout tak konkurenci, protože většina návštěvníků (přes 80%), přichází právě přes internetové vyhledávače.</p>
+
+
+          <div class=\"float_box question\">
+            <p class=\"title\"><strong>Co je to SEO?</strong></p>
+            <p>Za pomoci tohoto nástroje pro internetový marketing zajistíte Vašim webovým stránkám <strong>vyšší pozice ve vyhledávačích</strong>, zvýšíte tím počet návštěvníků, potenciálních zákazníků, a tím i výši Vašich zisků.</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
           </div>  
           <div class=\"text_box\">
             <h2 id=\"proc-je-seo-dulezite\">Proč je SEO tak důležité?</h2>
@@ -12,25 +20,40 @@
             <p>Zda-li potřebují Vaše stránky SEO optimalizaci si můžete snadno ověřit sami. Do libovolného vyhledávače zadejte klíčové slovo, odpovídající zaměření Vašich webových stránek. Pokud se odkaz na Vaše stránky objeví na první stránce, pokud možno co nejvýše, jsou vhodně optimalizované. Pozici lze ještě zvýšit, bývá to však mnohdy velmi náročné, a tím i drahé. Pokud se Vaše stránky objeví až na druhé (a další) straně, nebo dokonce vůbec, Vaše webové stránky potřebují SEO optimalizaci.</p>
             <h2 id=\"seo-kvalitni-webove-stranky\">SEO + Kvalitní webové stránky</h2>
             <p>První pozice ve vyhledávačích Vám zajistí přísun návštěvníků. Je však potřeba návštěvníka upoutat, nabídnout mu požadované služby či produkty a získat tak svého nového zákazníka. Pokud se návštěvník nebude cítit na Vašich webových stránkách dobře, nenajde co hledal, nezíská potřebné informace, nebudou mu Vaše stránky k užitku. Zákazník se z něj nestane a pravděpodobně se již na Vaše stránky nikdy nevrátí.</p>
-            <p>Nabídněte Vašim návštěvníkům ".mylink("pozadavky-kvalitni-stranky", "kvalitní webové stránky")."!</p>
-            <h2 id=\"zakladni-prvky\">Základní prvky SEO</h2>
+
+
+          <div class=\"float_box tip more\">
+            <p class=\"title\"><strong>".mylink("metody-seo", "Základní prvky SEO")."</strong></p>
             <ul>
-              <li><strong><acronym>Validní</acronym>, optimalizovaný a <acronym>sémanticky</acronym> správný kód</strong> stránek (např. <acronym>XHTML</acronym>, <acronym>CSS</acronym>)</li>
+              <li><strong><acronym>Validní</acronym> a optimalizovaný kód</li>
+              <li><acronym>Sémanticky</acronym> správný kód</strong> stránek</li>
               <li><strong>Analýza a optimalizace</strong> klíčových slov</li>
-              <li><strong>Optimalizace stránek</strong> pro potřeby vyhledávačů</li>
+              <li><strong>Optimalizace stránek</strong> pro vyhledávače</li>
               <li><strong>Registrace stránek</strong> do katalogů</li>
               <li><strong>Kvalitní <acronym>zpětné odkazy</acronym></strong></li>
               <li><strong>Kontrola</strong> úspěšnosti optimalizace</li>
             </ul>
-            <p class=\"read_next\">".mylink("metody-seo", "Metody pro SEO optimalizaci")."</p>
+            <div class=\"bottom\"><p class=\"read_next\">".mylink("metody-seo", "Metody pro SEO optimalizaci")."</p></div>
+          </div>  
+
+
+            <p>Nabídněte Vašim návštěvníkům ".mylink("pozadavky-kvalitni-stranky", "kvalitní webové stránky")."!</p>
             <h2 id=\"vseho-s-mirou\">Všeho s mírou</h2>
-            <p>SEO optimalizace by měla být provedena kvalitně a v rozumné míře tak, aby neomezovala a neškodila návštěvníkům. Pokud by se například na každé řádce v textu vyskytovalo stejné klíčové slovo, návštěvník by nečetl nic jíného. A dále, takové webové stránky by byly nejspíše penalizovány, tedy by si pozici ve vyhledávačích ještě zhoršily.</p>
+            <p>SEO optimalizace by měla být provedena kvalitně a v&nbsp;rozumné míře tak, aby neomezovala a neškodila návštěvníkům. Pokud by se například na každé řádce v&nbsp;textu vyskytovalo stejné klíčové slovo, návštěvník by nečetl nic jiného. A dále, takové webové stránky by byly nejspíše penalizovány, tedy by si pozici ve vyhledávačích ještě zhoršily.</p>
             <p>Webové stránky mají sloužit především lidem, nikoliv vyhledávačům.</p>
             <h2 id=\"seo-nemusi-byt-nutnosti\">SEO nemusí být vždy nutností</h2>
-            <p>SEO – optimalizace pro vyhledávače je jistě velmi silný nástroj pro zviditelnění Vašich webových stránek. Ne vždy je však takovéto zviditelnění žádoucí. Například intranetové stránky firmy, sloužící pouze pro interní záležitosti, nemusí být k nalezení. Tyto stránky mohou vyhledávačům zakazovat jejich <acronym>indexování</acronym> (ukládání) úplně. Mezi stránky, které nepotřebují mít dobré pozice ve vyhledávačích, by patřily některé soukromé stránky či stránky uzavřených skupin. Takové stránky pak slouží pouze jim.</p>
+            <p>SEO – optimalizace pro vyhledávače je jistě velmi silný nástroj pro zviditelnění Vašich webových stránek. Ne vždy je však takovéto zviditelnění žádoucí. Například intranetové stránky firmy, sloužící pouze pro interní záležitosti, nemusí být k&nbsp;nalezení. Tyto stránky mohou vyhledávačům zakazovat jejich <acronym>indexování</acronym> (ukládání) úplně. Mezi stránky, které nepotřebují mít dobré pozice ve vyhledávačích, by patřily některé soukromé stránky či stránky uzavřených skupin. Takové stránky pak slouží pouze jim.</p>
+
+
+          <div class=\"float_box question\">
+            <p class=\"title\"><strong>SEO - dnes již standard?</strong></p>
+            <p>Nutno zde upozornit, že SEO optimalizace se dnes stává <strong>spíše standardem než výsadou</strong>. Stránky, které jsou nyní na prvních místech výsledků vyhledávání, mohou být po nějaké době předstihnuty jinými.</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
             <h2 id=\"neseriozni-nabidka\">NEseriózní nabídka SEO optimalizace</h2>
-            <p>Jak poznat, že je nabídka SEO optimalizace seriózní a že se nejedná o podvod? To je velmi těžké. Základní informace o SEO optimalizaci jsou obecně známé. Detailní postupy a vlastní Know-How pro SEO si nechává každé webdesign studio či SEO-konzultant pro sebe a získává tak konkurenční výhodu. Asi nejlepším způsobem je ověření referencí daného studia. Jednoduše, zadáním klíčových slov (ty lze odhadnout podle zaměření hledané stránky) do vyhledávače, můžete zjistit, jak si daná stránka vede. Pokud bude u dané referenční webové stránky uvedeno, že byla provedena optimalizace SEO, měl by se odkaz na tuto stránku zobrazit mezi prvními ve výsledcích hledání.</p>
-            <p>Nutno zde upozornit, že <strong>SEO optimalizace se dnes stává spíše standardem</strong> než výsadou. Stránky, které jsou na prvních místech výsledků vyhledávání, mohou být po nějaké době předstihnuty jinými. Proto nelze věřit náhledům výsledků vyhledávání, které jsou mnohdy prezentovány na stránkách webdesignerů, ty v době shlédnutí již nemusí být aktuální.</p>
+            <p>Jak poznat, že je nabídka SEO optimalizace seriózní a že se nejedná o podvod? To je velmi těžké. Základní informace o SEO optimalizaci jsou obecně známé. Detailní postupy a vlastní Know-How pro SEO si nechává každé webdesign studio či SEO-konzultant pro sebe a získává tak konkurenční výhodu. Asi nejlepším způsobem je ověření referencí daného studia. Jednoduše, zadáním klíčových slov (ty lze odhadnout podle zaměření hledané stránky) do vyhledávače, můžete zjistit, jak si daná stránka vede. Pokud bude u&nbsp;dané referenční webové stránky uvedeno, že byla provedena optimalizace SEO, měl by se odkaz na tuto stránku zobrazit mezi prvními ve výsledcích hledání.</p>
           </div>".nl);
 
 ?>
