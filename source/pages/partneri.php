c13a65925d76700585b007e45ca2df4ba13a6fa7
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-13 07:04:45 +0000

    Skrytí stránky "Partneři".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@129 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/partneri.php
+++ source/pages/partneri.php
@@ -3,10 +3,6 @@
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
             <p>Zde bude seznam partnerů, spolupracujícíh subjektů apod.</p>
-          </div>  
-          <div class=\"text_box\">
-            <p>Lorem ipsum dolor sit amet consectetuer pellentesque quis justo ornare vel. Sed vitae felis tortor Phasellus urna quis ut tortor nibh id. Risus metus tortor quis elit sit Praesent elit tortor ac neque. Risus id condimentum pede et Proin Donec enim mauris pede nonummy. Lobortis lacus ultrices turpis et convallis Duis dui Curabitur convallis Integer. Diam id ipsum Lorem tortor.</p>
-            <p>Lorem tincidunt parturient Curabitur condimentum et eleifend quis ullamcorper id tellus. Congue eros tortor leo ac ante tincidunt sit Quisque consectetuer id. Tellus pede ipsum dictumst nascetur non cursus vitae tempus quis hendrerit. Sed adipiscing tellus nulla Proin id et vestibulum Morbi semper nec. Tellus nec Sed eu vel Morbi quis at urna a.</p>
           </div>".nl);
 
 ?>
