34de41bbbbcc4649ec6debdb2714b749bc4cea94
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-13 07:48:29 +0000

    Upravený obsah stránky "Tvorba webových stránek".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@134 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/nabidka-sluzeb/tvorba.php
+++ source/pages/nabidka-sluzeb/tvorba.php
@@ -3,17 +3,26 @@
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
             <p>Chcete vytvořit nové webové stránky? Potřebujete upravit Vaše stávající stránky? Mají Vaše stránky nízkou návštěvnost a chcete získat nové návštěvníky, potenciální zákazníky?</p>
+
+
+          <div class=\"float_box tip\">
+            <p class=\"title\"><strong>Nechte práci na nás</strong></p>
+            <p>Stačí Vám pouze nápad a odhodlání, vše ostatní již zařídíme my&hellip;</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
             <p>Vytvoříme Vám internetové stránky za použití moderních technologií, podle aktuálního trendu vývoje v oblasti prezentace na internetu. Samozřejmě budou Vaše stránky maximálně ".mylink("pouzitelnost-pristupnost", "přístupné a použitelné").", kód stránek <acronym>validní</acronym>, <acronym>sémanticky</acronym> správný. Součástí naší nabídky je i ".mylink("optimalizace-pro-vyhledavace", "optimalizace pro vyhledávače (SEO)").", ".mylink("registrace-do-katalogu", "registrace do katalogů").", poradenství a další&hellip;</p>
           </div>  
           <div class=\"text_box\">
             <h2 id=\"prezentace\">Prezentace na internetu</h2>
             <p>Základem prezentace na internetu jsou především ".mylink("pozadavky-kvalitni-stranky", "kvalitní webové stránky").". Co si představit pod tímto pojmem? Kvalitní webové stránky by měly být především obsahově hodnotné, doplněné pěkným a přehledným designem. Grafika by měla být doplňkem k Vaší prezentaci, nikoliv základem.</p>
-            <p>Našim cílem je Vám v prezentaci na Internetu pomoci&hellip;</p>
+            <p>Našim cílem je Vám v&nbsp;prezentaci na Internetu pomoci&hellip;</p>
             <h2 id=\"optimalizace-seo\">".mylink("optimalizace-pro-vyhledavace")."</h2>
-            <p>Stránky, které nikdo nenajde, jakoby nebyly. Našim cílem je stránky optimalizovat pomocí metod SEO tak, aby byly snadno k nalezení <acronym>vyhledávači</acronym> a vyskytovaly se na předních pozicích výsledků vyhledávání. Tato optimalizace je možná důležitější než vlastní web. Lze se velmi snadno zviditelnit a předstihnout tak konkurenci, protože většina návštěvníků (přes 80%), přichází právě přes internetové vyhledávače. ".mylink("optimalizace-pro-vyhledavace", "čtěte dále")."</p>
+            <p>Stránky, které nikdo nenajde, jakoby nebyly. Našim cílem je stránky optimalizovat pomocí metod SEO tak, aby byly snadno k&nbsp;nalezení <acronym>vyhledávači</acronym> a vyskytovaly se na předních pozicích výsledků vyhledávání. Tato optimalizace je možná důležitější než vlastní web. Lze se velmi snadno zviditelnit a předstihnout tak konkurenci, protože většina návštěvníků (přes 80%), přichází právě přes internetové vyhledávače. ".mylink("optimalizace-pro-vyhledavace", "čtěte dále")."</p>
             <h2 id=\"registrace-do-katalogu\">".mylink("registrace-do-katalogu")."</h2>
-            <p>Katalogy jsou jakousi moderní obdobou zlatých stránek v telefonním seznamu. Katalogy jsou určeny k přehlednému utřídění webových stránek podle kategorií. Vybráním kategorie, popř. okresu či jiných specifik, se postupně dostaneme až na seznam hledaných firem.</p>
-            <p>Mezi nejznámější české katalogy patří Seznam, Centrum a Atlas. Ze zahraničních katalogů jsou nejznámější Yahoo! a ODP. Existují také katalogy s daným zaměřením. ".mylink("registrace-do-katalogu", "čtěte dále")."</p>
+            <p>Katalogy jsou jakousi moderní obdobou zlatých stránek v&nbsp;telefonním seznamu. Katalogy jsou určeny k&nbsp;přehlednému utřídění webových stránek podle kategorií. Vybráním kategorie, popř. okresu či jiných specifik, se postupně dostaneme až na seznam hledaných firem.</p>
+            <p>Mezi nejznámější české katalogy patří Seznam, Centrum a Atlas. Ze zahraničních katalogů jsou nejznámější Yahoo! a ODP. Existují také katalogy s&nbsp;daným zaměřením. ".mylink("registrace-do-katalogu", "čtěte dále")."</p>
             <h2 id=\"registrace-domeny-hostingu\">".mylink("domeny-hosting", "Registrace domény a zřízení hostingu")."</h2>
             <p>Pokud máte webové stránky hotové, je potřeba je zveřejnit, umístit na internet. Stránky musí být někde fyzicky umístěné (hosting) a musí mít adresu (doména). Poté již stačí, když uživatel zadá Vaši webovou adresu a odpovědí mu budou právě Vaše stránky. ".mylink("domeny-hosting", "čtěte dále")."</p>
             <h2 id=\"redesign\">".mylink("redesign")."</h2>
@@ -27,8 +36,8 @@
             <p class=\"title\"><strong>Kam pokračovat dál?</strong></p>
             
             <ul>
-              <li>".mylink("pozadavky-kvalitni-stranky", "Co je to kvalitní webová stránka?")."</li>
               <li>".mylink("informace", "Chci více informací o webových stránkách")."</li>
+              <li>".mylink("pozadavky-kvalitni-stranky", "Co je to kvalitní webová stránka?")."</li>
               <li>".mylink("kontakt", "Chci kontaktovat společnost")."</li>
             </ul>  
               
@@ -36,7 +45,7 @@
           </div>  
 
             <h2 id=\"analyza-stranek\">Analýza stávajících stránek</h2>
-            <p>Máte své webové stránky? Chcete vědět, jestli vyhovují všem standardům? Zda jsou <acronym>validní</acronym>, <acronym>použitelné</acronym> a <acronym>přístupné</acronym>? Jak si stojí Vaše stránky ve <acronym>vyhledávačích</acronym>? Lze Vaše stránky najít v <acronym>katalozích</acronym>? Jsou tedy snadno nalezitelné? Vypracujeme pro Vás analýzu Vašich stávajících stránek&hellip;</p>
+            <p>Máte své webové stránky? Chcete vědět, jestli vyhovují všem standardům? Zda jsou <acronym>validní</acronym>, <acronym>použitelné</acronym> a <acronym>přístupné</acronym>? Jak si stojí Vaše stránky ve <acronym>vyhledávačích</acronym>? Lze Vaše stránky najít v&nbsp;<acronym>katalozích</acronym>? Jsou tedy snadno nalezitelné? Vypracujeme pro Vás analýzu Vašich stávajících stránek&hellip;</p>
             <h2 id=\"poradenstvi\">Poradenství</h2>
             <p>Chtěli byste více informací o webových stránkách? O prezentaci na internetu? Váháte, zda Vám vytvoření stránek pomůže, přinese nové klienty a zvýší zisky? Poskytneme Vám požadované informace a poradíme&hellip;</p>
             </p>
