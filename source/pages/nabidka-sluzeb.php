5059fed16cd1424d7dcd1d9b27a3f652e66f116b
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-13 07:34:35 +0000

    Upravený obsah stránky "Nabídka služeb".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@132 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/nabidka-sluzeb.php
+++ source/pages/nabidka-sluzeb.php
@@ -2,12 +2,21 @@
 
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
-            <p>Nabízíme Vám komplexní služby v oblasti ".mylink("tvorba", "prezentace na internetu")." a ".mylink("graficke-prace", "grafických prací").". Stačí Vám pouze nápad a odhodlání, vše ostatní již zařídíme my. Váš nápad převedeme do skutečnosti a vytvoříme Vám tak kvalitní prezentaci na internetu, doplněnou čistou a nápaditou grafikou. Dále Vám vytvoříme firemní grafický styl se vším, co budete potřebovat. Zejména se jedná o logo, vizitky, tiskoviny, reklamní letáčky a další.</p>
-            <p>Rádi Vám poskytneme kteroukoliv službu z naší nabídky samostatně. Můžete tak například požadovat pouze grafický návrh a stránky si naprogramovat sami.</p>
+            <p>Nabízíme Vám komplexní služby v&nbsp;oblasti ".mylink("tvorba", "prezentace na internetu")." a ".mylink("graficke-prace", "grafických prací").". <strong>Stačí Vám pouze nápad a odhodlání</strong>, vše ostatní již zařídíme my. Váš nápad převedeme do skutečnosti a vytvoříme Vám tak kvalitní prezentaci na internetu, doplněnou čistou a nápaditou grafikou. Dále Vám vytvoříme firemní grafický styl se vším, co budete potřebovat. Zejména se jedná o logo, vizitky, tiskoviny, reklamní letáčky a další.</p>
+            <p>Rádi Vám poskytneme kteroukoliv službu z&nbsp;naší nabídky samostatně. Můžete tak například požadovat pouze grafický návrh a stránky si naprogramovat sami.</p>
           </div>  
           <div class=\"text_box\">
+
+          
+          <div class=\"float_box tip more\">
+            <p class=\"title\"><strong>".mylink("reference", "Naše reference")."</strong></p>
+            <p>Jsme hrdi na svoji práci. Však, posuďte sami a přesvědčte se o kvalitě naší práce&hellip;</p>
+            <div class=\"bottom\"><p class=\"read_next\">".mylink("reference", "Prohlédnout reference")."</p></div>
+          </div>  
+
+
             <h2 id=\"tvorba-webovych-stranek\">".mylink("tvorba")."</h2>
-            <p>Tvoříme ".mylink("pozadavky-kvalitni-stranky", "kvalitní webové stránky")." za použití moderních technologií s přihlédnutím k aktuálnímu trendu vývoje v oblasti prezentace na internetu. Vaše stránky budou maximálně <acronym>přístupné</acronym> a <acronym>použitelné</acronym>, kód stránek <acronym>validní</acronym>, <acronym>sémanticky</acronym> správný. Součástí naší nabídky je i optimalizace pro vyhledávače (<acronym>SEO</acronym>), registrace do internetových <acronym>katalogů</acronym>, poradenství a další&hellip; ".mylink("tvorba", "čtěte dále")."</p>
+            <p>Tvoříme ".mylink("pozadavky-kvalitni-stranky", "kvalitní webové stránky")." za použití moderních technologií s&nbsp;přihlédnutím k&nbsp;aktuálnímu trendu vývoje v&nbsp;oblasti prezentace na internetu. Vaše stránky budou maximálně <acronym>přístupné</acronym> a <acronym>použitelné</acronym>, kód stránek <acronym>validní</acronym>, <acronym>sémanticky</acronym> správné. Součástí naší nabídky je i optimalizace pro vyhledávače (<acronym>SEO</acronym>), registrace do internetových <acronym>katalogů</acronym>, poradenství a další&hellip; ".mylink("tvorba", "čtěte dále")."</p>
             <ul class=\"link_farm\">
               <li>".mylink("optimalizace-pro-vyhledavace")."</li>
               <li>".mylink("registrace-do-katalogu")."</li>  
@@ -16,7 +25,7 @@
               <li>".mylink("nasledna-sprava")."</li>
             </ul>
             <h2 id=\"graficke-prace\">".mylink("graficke-prace")."</h2>
-            <p>Nabízíme Vám nejen stránky s kvalitním, svěžím a originálním vzhledem, ale i široké spektrum grafických služeb. Zejména návrhy různých forem tištěné a počítačové grafiky, včetně 3D vizualizací. ".mylink("graficke-prace", "čtěte dále")."</p>
+            <p>Nabízíme Vám nejen stránky s&nbsp;kvalitním, svěžím a originálním vzhledem, ale i široké spektrum grafických služeb. Zejména návrhy různých forem tištěné a počítačové grafiky, včetně 3D vizualizací. ".mylink("graficke-prace", "čtěte dále")."</p>
             <ul class=\"link_farm\">
               <li>".mylink("graficke-prace", "Corporate design (firemní grafický styl)", "corporate-design", "Corporate design (firemní grafický styl)")."</li>
               <li>".mylink("graficke-prace", "Prezentace na CD, DVD", "prezentace-cd-dvd", "Prezentace na CD, DVD")."</li>
