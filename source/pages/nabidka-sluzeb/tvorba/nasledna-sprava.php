69f137b468646e2f06f6169c68f16be00ecd1ffb
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-13 23:39:03 +0000

    Upravený obsah stránky "Následná správa webových stránek".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@152 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/nabidka-sluzeb/tvorba/nasledna-sprava.php
+++ source/pages/nabidka-sluzeb/tvorba/nasledna-sprava.php
@@ -6,11 +6,21 @@
           </div>  
           <div class=\"text_box\">
             <h2 id=\"aktualizace-stranek\">Aktualizace webových stránek</h2>
-            <p>Internet je jedno z nejrychlejších médií. Je proto důležité, udržovat Vaše webové stránky <strong>stále aktuální</strong>. V žádném případě by se na stránkách neměla vyskytnout neaktuální informace.</p>
-            <p>Uveďme si jednoduchý příklad: Představte si, že máte uvedeno: &bdquo;Přijmeme řidiče, nástup možný ihned.&ldquo; Datum vložení - rok ".(date('Y') - (date('m') > 3 ? 1 : 2)).". Je to nesmyslné! Snadno tak přijdete o důvěru. Buďto si návštěvník pomyslí, že Vaše firma nebyla schopná si za celý rok sehnat zaměstnance, nebo že Vaše firma není schopná aktualizovat informace na vlastních webových stránkách. <strong>Návštěvníci očekávají změnu a aktuálnost.</strong> Záměrem je, aby se návštěvníci opět (a rádi) vraceli. Dejte jim k tomu důvod.</p>
+            <p>Internet je jedno z&nbsp;nejrychlejších médií. Je proto důležité, udržovat Vaše webové stránky <strong>stále aktuální</strong>. V&nbsp;žádném případě by se na stránkách neměla vyskytnout neaktuální informace.</p>
+
+
+          <div class=\"float_box info\">
+            <div class=\"title\"><strong>Aktuální obsah</strong></div>
+            <p>Návštěvníci očekávají <strong>změnu a aktuálnost.</strong></p>
+            <p>Záměrem je, aby se návštěvníci opět (a rádi) vraceli. Dejte jim k&nbsp;tomu důvod&hellip;</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+
+            <p>Uveďme si jednoduchý příklad: Představte si, že máte uvedeno: &bdquo;Přijmeme řidiče, nástup možný ihned.&ldquo; Datum vložení - rok ".(date('Y') - (date('m') > 3 ? 1 : 2)).". Je to nesmyslné! Snadno tak přijdete o důvěru. Buďto si návštěvník pomyslí, že Vaše firma nebyla schopná si za celý rok sehnat zaměstnance, nebo že Vaše firma není schopná aktualizovat informace na vlastních webových stránkách.</p>
             <h2 id=\"statistiky\">Statistiky</h2>
             <p>Spokojení návštěvníci jsou výsledkem ".mylink("pozadavky-kvalitni-stranky", "kvalitních webových stránek").". Jistě Vás budou zajímat statistiky o návštěvnosti. <strong>Kolik je návštěvníků a odkud přicházejí?</strong> Jak dlouho se na stránkách pohybují? Jak se pohybují? Co jim činí problémy a co je potěšilo? Jaké prohlížeče používají? Jak si stojí Vaše stránky ve vyhledávačích?</p>
-            <p>Na základě těchto statistik se s webovými stránkami dále pracuje (úpravy, rozšíření&hellip;).</p>
+            <p>Na základě těchto statistik se s&nbsp;webovými stránkami dále pracuje (úpravy, rozšíření&hellip;).</p>
           </div>".nl);
 
 ?>
