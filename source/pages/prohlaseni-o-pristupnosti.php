ffac5e778a415b8102742ebffc1cc84fab27250b
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-14 00:27:34 +0000

    Upravený obsah stránky "Prohlášení o přístupnosti".
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@154 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/prohlaseni-o-pristupnosti.php
+++ source/pages/prohlaseni-o-pristupnosti.php
@@ -2,12 +2,23 @@
 
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
-            <p>Zde bude prohlášení o přístupnosti.</p>
           </div>  
           <div class=\"text_box\">
-            <p>Lorem ipsum dolor sit amet consectetuer consequat feugiat sit Nulla condimentum. Nisl id Aenean molestie ridiculus dui ligula libero Vestibulum natoque pede. Nibh interdum justo et Phasellus non libero odio Proin lobortis orci. Vestibulum ac nibh semper adipiscing Vestibulum cursus quis ut cursus vel. Tempus habitant nibh magna eu turpis Nam felis ullamcorper Integer sed. Eget Integer dui risus a eget tortor quis fringilla Suspendisse tempor. Turpis Curabitur.</p>
-            <p>Curabitur tincidunt id id gravida Suspendisse tristique metus metus leo Curabitur. Orci Phasellus porttitor Curabitur adipiscing Aenean justo quis eu ut nulla. Tellus mi eu Phasellus Aliquam pede felis eros Phasellus Phasellus semper. Vitae ut metus cursus Vestibulum egestas nisl eros interdum congue ac. Urna vel Donec et est Sed ultrices eleifend Curabitur a dapibus. Pede ligula quis lacinia Vestibulum fermentum laoreet scelerisque mattis nunc felis. Curabitur euismod nec.</p>
-            <p>Lorem gravida elit pretium Curabitur porttitor Nulla ac metus ante platea. Quis vitae pellentesque et ut vitae ipsum Nam quam magnis nec. Consectetuer habitasse orci accumsan elit venenatis enim Aenean Curabitur Suspendisse tempus. Nullam metus dolor Maecenas at aliquet ligula suscipit nonummy convallis ut. Tellus Morbi ipsum pretium neque tellus nibh consectetuer adipiscing ipsum.</p>
+            <p>Tyto webové stránky byly připraveny s&nbsp;ohledem k&nbsp;přístupnosti a bezbariérovosti webu tak, aby splňovaly zásady přístupnosti podle metodik <a href=\"http://www.blindfriendly.cz/\" title=\"Přístupnost stránek pro nevidomé a slabozraké\">Blind Friendly Web</a>,  <a href=\"http://www.pristupnost.cz/wcag/\" title=\"Pravidla WCAG\">WCAG 1.0</a> a <a href=\"http://www.pravidla-pristupnosti.cz/\" title=\"Pravidla přístupného webu\">Pravidel tvorby přístupného webu</a>.</p>
+
+
+          <div class=\"float_box question more\">
+            <div class=\"title\"><strong>".mylink("pouzitelnost-pristupnost", "Co je přístupnost webu?", "pristupnost")."</strong></div>
+            <p>Nevíte, co si představit pod pojmem přístupnost webové stránky? </p>
+            <div class=\"bottom\"><p class=\"read_next\">".mylink("pouzitelnost-pristupnost", "Více informací o přístupnosti", "pristupnost")."</p></div>
+          </div>  
+
+
+            <p>Web je vytvořen podle specifikace <a href=\"http://www.w3.org/TR/xhtml1/\" title=\"Specifikace standardu XHTML 1.0\">XHTML&nbsp;1.0 Strict</a> a dodržuje syntaktickou a sémantickou správnost. Obsah je kompletně oddělen od stylu pomocí <a href=\"http://www.w3.org/TR/CSS21/\" title=\"Specifikace standardu CSS 2.1\">CSS</a>, díky tomu je možné web prezentovat na dalších výstupních zařízeních. Velikost písma je definovaná v&nbsp;relativních jednotkách, takže písmo se dá zvětšovat i zmenšovat pomocí nastavení prohlížeče.</p>
+            <h2>Doporučená rozšíření prohlížeče</h2>
+            <p>Ve <a href=\"http://cs.wikipedia.org/wiki/Portable_Document_Format\" title=\"Portable Document Format (přenosný formát dokumentu)\">formátu PDF</a> nabízíme některé doplňující informace, které jsou příliš rozsáhlé nebo graficky zpracované a je vhodné jejich stažení. Pro zobrazení těchto dokumentů je třeba mít nainstalovaný <a href=\"http://get.adobe.com/reader/\" title=\"Klepnutím stáhněte prohlížeč Adobe Reader\">prohlížeč Adobe Reader</a>, který je zdarma ke stažení na stránkách společnosti Adobe Systems, Inc..</p>
+            <h2>Kontakt na správce webu</h2>
+            <p>Vaše náměty, postřehy či informace o problémech se zobrazováním těchto stránek prosím zasílejte na adresu správce serveru <a href=\"mailto:info@koldasoft.cz\" title=\"Poslat e-mail na info@koldasoft.cz\">info@koldasoft.cz</a>. Využít můžete také ".(mylink("kontakt", "kontaktní formulář", "contactform_float_box")).".</p>
           </div>".nl);
 
 ?>
