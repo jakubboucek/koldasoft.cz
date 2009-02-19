a0d32c1e7be78005c96fcad7f7df43b3ce471875
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-19 16:05:23 +0000

    Doplnění stránky "Náš tým". Panorama foto, nové portréty, odkazy více.
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@164 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/o-nas/nas-tym.php
+++ source/pages/o-nas/nas-tym.php
@@ -4,9 +4,8 @@
             
             <h1>$page_name</h1>
             <p>Máme rádi, když víme, s&nbsp;kým jednáme. Jsme tým zkušených odborníků s&nbsp;nadšením pro věc. Jsme mladí, ambiciózní a víme, co chceme.</p>
-            <img src=\"".$cesta."pictures/koldasoft_team_photo_01.jpg\" height=\"223\" width=\"630\" alt=\"Náš tým\" />
+            <a href=\"".$cesta."pictures/koldasoft_team_photo.jpg\" title=\"Otevřít fotografii v plném rozlišení\"><img src=\"".$cesta."pictures/koldasoft_team_photo_panorama.jpg\" height=\"393\" width=\"630\" alt=\"Foto: Tým Koldasoft\" /></a>
             <p>Nabízíme Vám seznam členů našeho týmu a potřebné kontaktní údaje. Neváhejte se obrátit na kohokoliv z&nbsp;nás - velmi rádi Vám poradíme a pomůžeme.</p>
-            
           </div>  
 
           <div class=\"cleaner\"></div>
@@ -14,70 +13,70 @@
           <div class=\"text_box\">
             <div class=\"member\">
               <h2 id=\"kolda\">".(mylink("jiri-kolarik", "Jiří Kolařík - <em>ředitel společnosti</em>"))."</h2>
-              ".(mylink("jiri-kolarik", "<img src=\"".$cesta."pictures/portrait_kolda.jpg\" alt=\"Jiří Kolařík\" width=\"115\" height=\"139\" />"))."
+              ".(mylink("jiri-kolarik", "<img src=\"".$cesta."pictures/portrait_jiri_kolarik.jpg\" alt=\"Foto: Jiří Kolařík\" width=\"115\" height=\"139\" />"))."
               <ul>
                 <li><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;00</li>
                 <li><strong>E-mail:</strong> <a href=\"mailto:jiri.kolarik@koldasoft.cz\" title=\"\">jiri.kolarik@koldasoft.cz</a></li>
                 <li><strong>ICQ:</strong> 126-976-186</li>
                 <li><strong>Skype:</strong> jiri.kolda</li>
                 <li><a href=\"".$cesta."files/koldasoft_vizitka_kolarik_v2.1.pdf\" title=\"Vizitka Jiří Kolařík (formát PDF, 32 kB)\">Vizitka</a></li>
-                <li>".(mylink("jiri-kolarik", "Více informací"))."</li>
               </ul>
-              <p>Chcete obchodovat? Chcete realizovat zajímavý projekt? Chcete se prosadit? Tomu říkám výzva a nic pro mě není překážkou...<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"about\">Chcete obchodovat? Chcete realizovat zajímavý projekt? Chcete se prosadit? Tomu říkám výzva a nic pro mě není překážkou...<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"more read_next\">".(mylink("jiri-kolarik", "Více informací"))."</p>
             </div>
 
             <div class=\"member\">
               <h2 id=\"lucka\">".(mylink("lucie-brandyska", "Lucie Brandýská - <em>asistentka ředitele</em>"))."</h2>
-              ".(mylink("lucie-brandyska", "<img src=\"".$cesta."pictures/portrait_lucka.jpg\" alt=\"Lucie Brandýská\" width=\"115\" height=\"139\" />"))."
+              ".(mylink("lucie-brandyska", "<img src=\"".$cesta."pictures/portrait_lucie_brandyska.jpg\" alt=\"Foto: Lucie Brandýská\" width=\"115\" height=\"139\" />"))."
               <ul>
                 <li><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;10</li>
                 <li><strong>E-mail:</strong> <a href=\"mailto:lucie.brandyska@koldasoft.cz\" title=\"\">lucie.brandyska@koldasoft.cz</a></li>
                 <li><strong>ICQ:</strong> 212-188-049</li>
                 <li><a href=\"".$cesta."files/koldasoft_vizitka_brandyska_v2.1.pdf\" title=\"Vizitka Radek Věchet (formát PDF, 32 kB)\">Vizitka</a></li>
-                <li>".(mylink("lucie-brandyska", "Více informací"))."</li>
               </ul>
-              <p>Fakturu připravím na počkání, zázraky do druhého dne! A kafíčko k&nbsp;tomu...<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"about\">Fakturu připravím na počkání, zázraky do druhého dne! A kafíčko k&nbsp;tomu...<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"more read_next\">".(mylink("lucie-brandyska", "Více informací"))."</p>
             </div>
             <div class=\"cleaner\"></div>
             <div class=\"member\">
               <h2 id=\"morty\">".(mylink("radek-vechet", "Radek Věchet - <em>hlavní grafik</em>"))."</h2>
-              ".(mylink("radek-vechet", "<img src=\"".$cesta."pictures/portrait_radek.jpg\" alt=\"Radek Věchet\" width=\"115\" height=\"139\" />"))."
+              ".(mylink("radek-vechet", "<img src=\"".$cesta."pictures/portrait_radek_vechet.jpg\" alt=\"Foto: Radek Věchet\" width=\"115\" height=\"139\" />"))."
               <ul>
                 <li><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;20</li>
                 <li><strong>E-mail:</strong> <a href=\"mailto:radek.vechet@koldasoft.cz\" title=\"\">radek.vechet@koldasoft.cz</a></li>
                 <li><strong>ICQ:</strong> 315-739-033</li>
                 <li><strong>Skype:</strong> xxx_radek_xxx</li>
                 <li><a href=\"".$cesta."files/koldasoft_vizitka_vechet_v2.1.pdf\" title=\"Vizitka Radek Věchet (formát PDF, 32 kB)\">Vizitka</a></li>
-                <li>".(mylink("radek-vechet", "Více informací"))."</li>
               </ul>
-              <p>Nalezení kompromisu mezi krásným návrhem a jeho techickým provedením je mojí posedlostí.<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"about\">Nalezení kompromisu mezi krásným návrhem a jeho techickým provedením je mojí posedlostí.<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"more read_next\">".(mylink("radek-vechet", "Více informací"))."</p>
             </div>
             <div class=\"cleaner\"></div>
             <div class=\"member\">
               <h2 id=\"bukaj\">".(mylink("jakub-boucek", "Jakub Bouček - <em>hlavní programátor</em>"))."</h2>
-              ".(mylink("jakub-boucek", "<img src=\"".$cesta."pictures/portrait_jakub.jpg\" alt=\"Jakub Bouček\" width=\"115\" height=\"139\" />"))."
+              ".(mylink("jakub-boucek", "<img src=\"".$cesta."pictures/portrait_jakub_boucek.jpg\" alt=\"Foto: Jakub Bouček\" width=\"115\" height=\"139\" />"))."
               <ul>
                 <li><strong>Mobil:</strong> (+420) 775&nbsp;77&nbsp;15&nbsp;30</li>
                 <li><strong>E-mail:</strong> <a href=\"mailto:jakub.boucek@koldasoft.cz\" title=\"\">jakub.boucek@koldasoft.cz</a></li>
                 <li><strong>ICQ:</strong> 149-366-049</li>
                 <li><strong>Skype:</strong> jakub.boucek.info</li>
                 <li><a href=\"".$cesta."files/koldasoft_vizitka_boucek_v2.1.pdf\" title=\"Vizitka Jakub Bouček (formát PDF, 32 kB)\">Vizitka</a></li>
-                <li>".(mylink("jakub-boucek", "Více informací"))."</li>
               </ul>
-              <p>Jestli je někde něco, co se dá naprogramovat, pusťe mě k&nbsp;tomu. Není nic, do čeho bych nedokázal napsat alespoň ".'$kousek_kodu'.".<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"about\">Jestli je někde něco, co se dá naprogramovat, pusťe mě k&nbsp;tomu. Není nic, do čeho bych nedokázal napsat alespoň ".'$kousek_kodu'.".<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"more read_next\">".(mylink("jakub-boucek", "Více informací"))."</p>
             </div>
             <div class=\"cleaner\"></div>
             <div class=\"member\">
               <h2 id=\"harem_master\">".(mylink("jiri-drahorad", "Jiří Drahorád - <em>designer</em>"))."</h2>
-              ".(mylink("jiri-drahorad", "<img src=\"".$cesta."pictures/portrait_jiri.jpg\" alt=\"Jiří Drahorád\" width=\"115\" height=\"139\" />"))."
+              ".(mylink("jiri-drahorad", "<img src=\"".$cesta."pictures/portrait_jiri_drahorad.jpg\" alt=\"Foto: Jiří Drahorád\" width=\"115\" height=\"139\" />"))."
               <ul>
                 <li><strong>Mobil:</strong> (+420) 720&nbsp;24&nbsp;92&nbsp;92</li>
                 <li><strong>E-mail:</strong> <a href=\"mailto:jiri.drahorad@koldasoft.cz\" title=\"\">jiri.drahorad@koldasoft.cz</a></li>
                 <li><strong>ICQ:</strong> 233-445-142</li>
                 <li><a href=\"".$cesta."files/koldasoft_vizitka_drahorad_v2.1.pdf\" title=\"Vizitka Jiří Drahorád (formát PDF, 32 kB)\">Vizitka</a></li>
-                <li>".(mylink("jiri-drahorad", "Více informací"))."</li>
               </ul>
-              <p>Mám rád lidi kolem sebe. Mám rád, když jich je alespoň 64: krásných a mladých žen. Mám rád svůj harém&hellip;<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"about\">Mám rád lidi kolem sebe. Mám rád, když jich je alespoň 64: krásných a mladých žen. Mám rád svůj harém&hellip;<span class=\"cite\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><div class=\"end\">&nbsp;</div></p>
+              <p class=\"more read_next\">".(mylink("jiri-drahorad", "Více informací"))."</p>
             </div>
           </div>".nl);
 
