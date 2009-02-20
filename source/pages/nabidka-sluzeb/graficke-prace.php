a978a793c36f04a3d5b137a72d044d9e0b8212c2
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-20 10:25:38 +0000

    Nový obsah grafických služeb.
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@167 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/nabidka-sluzeb/graficke-prace.php
+++ source/pages/nabidka-sluzeb/graficke-prace.php
@@ -1,26 +1,135 @@
-<?php  //Graficke prace
+  <?php  //Graficke prace
 
   echo("          <div class=\"text_box\">
             <h1>$page_name</h1>
             <p>Nabízíme Vám nejen stránky s&nbsp;kvalitním, svěžím a originálním webdesignem, ale i široké spektrum grafických služeb. Zejména návrhy různých forem tištěné a počítačové grafiky, včetně 3D vizualizací.</p>
           </div>  
+
           <div class=\"text_box\">
-            <h2 id=\"corporate-design\">Corporate design (firemní grafický styl)</h2>
-            <p>Toto anglické slovní spojení znamená firemní grafický styl neboli &bdquo;<em>tvář firmy</em>&ldquo;, kterou se firma prezentuje na veřejnosti. Základem corporate designu je grafický manuál, který obsahuje logotyp (značku) firmy a popisuje, jak toto logo a prvky s&nbsp;ním související správně aplikovat v&nbsp;praxi a jak s&nbsp;ním naopak nezacházet.</p>
-            <p><strong>Grafický manuál</strong> obsahuje zejména základní barevnost a barevné varianty loga, písmo, přesné proporce, velikost, ochrannou zónu a také tzv. zakázaný styl – co se s&nbsp;logem nemá dělat (měnit poměry stran, rotovat, přebarvovat, atp.). Dále jsou zde uvedeny konkrétní aplikace loga - vizitky, hlavičkové papíry, razítka, vzory smluv, faktury, obálky, reklamní předměty, potisky automobilů, &hellip;</p>
-            <p>Stanovení a dodržování jednotného firemního vzhledu, tištěných i elektronických dokumentů, zvýší firemní kulturu, <strong>jednotnost a prestiž</strong> Vaší organizace.</p>
-            <h2 id=\"prezentace-cd-dvd\">Prezentace na CD, DVD</h2>
-            <p>Potřebujete prezentovat Vaši společnost na tiskové konferenci nebo obchodním partnerům při jednání? Chystáte se na veletrh a nevíte čím zaujmout potencionální zákazníky? Nestačí Vám standardní nástroje na vytváření prezentací nebo jim jen neumíte dát tu správnou formu?</p>
-            <p>Vytvoříme Vám prezentaci přesně na míru, která posluchače <strong>upoutá a zaujme</strong> na první pohled. Multimediální obsah jako krátké animace, videa nebo nevtíravá hudba v&nbsp;pozadí, doplněná několika interaktivními zvukovými efekty, je samozřejmostí. Výslednou prezentaci dodáme na samospustitelném CD nebo video DVD. Disky je možné přehrávat na běžném PC (není potřeba nic instalovat ani nastavovat) nebo využít stolního DVD přehrávače. Disky můžeme dodat i s&nbsp;potiskem, obalem a bookletem.</p>
+
+
+            <h2 id=\"webdesign\">Webdesign</h2>
+            <p>Je vždy radost pohledět na pěknou webovou stránku s lehkou a osvěžující grafikou, která Vás <strong>zaujme</strong> a která Vám <strong>nebrání v orientaci</strong> na stránkách. Zvýší se tím pravděpodobnost dalších návštěv a zaručí Vám spokojené uživatele Vašich webových stránek. Už první pohled dokáže návštěvníka podvědomě naladit na co se má připravit. Důležitou součástí celkového dojmu není jen hlavička a hlavní menu ale i tak drobné detaily jako odrážka, odkaz na další stránku nebo ikonka v adresním řádku prohlížeče.</p>
+            
+            <h3 id=\"webdesign-navrh\">Návrh</h3>
+            <p>Návrh webových stránek vychází vždy z <strong>analýzy požadavků</strong> našeho klienta a corporate designu společnosti. Ctí firemní barvy a vhodně doplňuje grafický styl společnosti. Z toho vyplývá, že každý web by měl být <strong>vizuálně unikátní</strong>. První fází návrhu je celkový layout stránek neboli grafické ztvárnění jejich rozložení, které je definováno v analýze projektu. Do toho spadá především pozadí celého webu, hlavička, menu, navigace, rozdělení obsahové části na sloupce, pozadí obsahové části a patička. V druhé fázi se navrhuje vlastní obsah webu, který tvoří odstavce textu, informační boxy, obrázky, odkazy, seznamy apod. Všechny tyto části dávají dohromady <strong>promyšlený celek</strong>, který je průběžně konzultován s klientem.</p>
+
+            <h3 id=\"rozrezani-grafiky\">Rozřezání grafiky</h3>
+            <p>Jedná se o finální část výrobního procesu návrhu webové stránky, kdy je schválený návrh rozřezán a vyexportován do jednotlivých drobných částí, které poté dostane kodér k vytvoření kaskádového stylu (CSS) stránky.  Při tomto procesu se dbá především na udržení <strong>kvality výsledných obrázků</strong> při co <strong>nejnižší datovém objemu</strong>.</p>
+
+          <div class=\"float_box question\">
+            <p class=\"title\"><strong>Chcete váš web oživit?</strong></p>
+            <p>Chcete, aby váš web zákazníky oslovil? Přáli byste si mít animovanou hlavičku, interaktivní galerii nebo do vašeho webu vložit video? Pak budete potřebujete doplňky vytvořené technologií Flash.</p>
+            <div class=\"bottom\"></div>
+          </div>  
+          
+          
+            <h2 id=\"corporate-design\">Corporate design</h2>
+            <p>Toto anglické slovní spojení znamená firemní grafický styl neboli „tvář firmy“, kterou se firma prezentuje na veřejnosti. Základem corporate designu je grafický manuál, který obsahuje logotyp (značku) firmy a popisuje, jak toto logo a prvky s ním související správně aplikovat v praxi a jak s ním naopak nezacházet.</p>
+            <h3 id=\"logotyp-znacka\">Logotyp, značka</h3>
+            <p>Základem celého corporate designu je vlastní <strong>originální značka</strong>, která společnost všude doprovází a prezentuje. Pokud je kvalitně a s rozvahou navržená, dá se poté využít na téměř <strong>jakémkoli médiu</strong>. Její barevnost zpravidla definuje firemní barvy. Podrobné vlastnosti logotypu jsou rozebrány v logomanuálu společnosti. Zde si můžete přečíst, co vše by měl obsahovat.</p>
+            <h3 id=\"logomanual\">Logomanuál</h3>
+            <ul>
+              <li>Logotyp</li>
+              <li>Verze a barevné verze logotypu</li>
+              <li>Definice firemních barev</li>
+              <li>Stanovení proporcí logotypu</li>
+              <li>Definice ochranné zóny logotypu</li>
+              <li>Stanovení velikostí logotypu</li>
+              <li>Zakázaný styl</li>
+              <li>Firemní písmová rodina</li>
+            </ul>
+            <p>Stanovení a dodržování jednotného firemního vzhledu, tištěných i elektronických dokumentů, zvýší firemní kulturu, <strong>jednotnost a prestiž Vaší organizace</strong>. Proto logomanuál obvykle obsahuje další aplikace značky. Jako příklad zde některé uvádíme:</p>
+            <ul>
+              <li>Aplikace logotypu</li>
+              <li>Vizitka</li>
+              <li>Hlavičkový papír
+              <ul>
+                <li>Šablona firemního dokumentu</li>
+                <li>Šablona faktury</li>
+              </ul></li>
+              <li>Obálka</li>
+              <li>Razítko</li>
+              <li>Potisk firemního vozidla</li>
+              <li>Šablona e-mailu</li>
+              <li>Šablona prezentace</li>
+              <li>Identifikační karta zaměstnance</li>
+              <li>Označení kanceláře</li>
+              <li>Reklamní předměty
+                <ul>
+                  <li>Propiska</li>
+                  <li>Tričko</li>
+                  <li>Hrníček</li>
+                  <li>Trhací bloček</li>
+                  <li>Deštník</li>
+                </ul></li>  
+            </ul>  
+          
+
+
+
             <h2 id=\"tistena-grafika\">Tištěná grafika</h2>
-            <p>Každá firma či organizace potřebuje nějaké tiskoviny. Nezáleží na tom, zda se jedná o ceníky a katalogy výrobků, ročenku o hospodaření, novoroční pozdrav obchodním partnerům, letáky, plakáty, trička, samolepky pro reklamní kampaně, brožury k&nbsp;produktům nebo prostě jen vizitky. Vše Vám zhotovíme dle Vašich přání a za dodržení Vašeho ".mylink("graficke-prace", "firemního grafického stylu", "corporation-design", "Corporate design (firemní grafický styl)").".</p> 
-            <p>V&nbsp;závislosti na Vašich požadavcích a možnostech navrhneme typ papíru (gramáž), jeho povrchovou úpravu (lesklý, matný) a popř. další vlastnosti (průsvitný, barevný). Zajistíme všechny náležitosti potřebné k&nbsp;tisku (zejména barvy a vhodný formát).</p>
-            <h2 id=\"pocitacova-grafika\">Počítačová grafika</h2>
-            <p>Připadají Vám Vaše webové stránky příliš obyčejné, chudé a nudné? Rádi byste animovaný banner pro internetovou reklamní kampaň, animované intro, slideshow (malá galerie, sada obrázků) nebo krátké video? Chcete <strong>zaujmout a rozptýlit</strong> návštěvníky webových stránek jednoduchou zábavnou hrou? Chcete nabídnout Vašim návštěvníkům možnost stáhnout si originální pozadí na plochu či spořič obrazovky s&nbsp;motivem Vaší firmy.</p>
-            <p>To vše je práce pro nás&hellip;</p>
+            <p>Tištěná grafika zahrnuje prakticky všechno, co se dá vytisknout na papír (nebo jiná média) či vyřezat a následně nalepit. I v digitálním věku se prakticky žádná společnost neobejde bez faktur, dopisů nebo vizitek. Specifika tištěné grafiky jsou zejména <strong>typografie a sazba</strong>, úprava pro barvový prostor CMYK a následně <strong>příprava pro tisk</strong> (tzv. prepress).</p>
+            <p>Pro lepší představu zde nabízíme popis a ukázky několika tištěných dokumentů, které naši klienti často požadují.</p>
+
+          <div class=\"float_box tip\">
+            <p class=\"title\"><strong>Potřebujete corporate design?</strong></p>
+            <p>Corporate design znamená firemní grafický styl neboli „tvář firmy“, kterou se firma prezentuje na veřejnosti. Základem corporate designu je grafický manuál, který obsahuje logotyp (značku) firmy a popisuje, jak toto logo a prvky s ním související správně aplikovat v praxi a jak s ním naopak nezacházet.</p>
+            <div class=\"bottom\"></div>
+          </div>  
+
+            <h3 id=\"vizitky\">Vizitky</h3>
+            <p>Nejsnazší a nejreprezentativnější způsob, jak někomu rychle předat své kontaktní informace je pomocí pěkné a přehledné vizitky. Taková vizitka se vyznačuje především <strong>dobrou čitelností</strong>, jasně <strong>strukturovaným obsahem</strong>, kvalitně provedeným tiskem i ořezem na papír o patřičné tvrdosti (gramáži) a povrchové úpravě (lesk, mat atd.)</p>
+
+            <h3 id=\"hlavickove-papiry\">Hlavičkové papíry</h3>
+            <p>Oficiální dopis, smlouvu nebo fakturu by měla společnost prezentovat  na dokumentu odpovídajícím jeho váze. Zpravidla obsahuje kromě <strong>značky</strong> také nejdůležitější <strong>kontaktní informace</strong>, které by ovšem neměli narušovat celkový estetický dojem z dokumentu. Pomocí hlavičkových papíru lze také jasně odlišit, jestli dokument obsahuje veřejné, interní nebo utajované informace. Jedná se o velkonákladové tisky pomocí offsetových technologií.</p>
+
+            <h3 id=\"reklamni-letaky\">Reklamní letáky a reklama do časopisů</h3>
+            <p>Běžná a rozšířená forma reklamy je vkládání reklamních bloků či celých stran do časopisů a novin. Lze tak snadno zacílit na konkrétní <strong>cílovou skupinu</strong> obyvatelstva (např. hobby a lifestyle magazíny) i širokou veřejnost (celostátní denníky). Letáky rozdávané pouze klientům nebo široké veřejnosti poutají pozornost nejen při oficiálních <strong>promo akcích</strong> a <strong>veletrzích</strong> ale i … Provedení závisí konkrétní zakázce.</p>
+
+            <h3 id=\"reprezentacni-tiskoviny\">Reprezentační tiskoviny</h3>
+            <p>Jedná se o výjimečné dokumenty typu novoročenka apod . Bývají tištěny na dražší papíry s vyšší tvrdostí (gramáží) a povrchovou úpravou (lesk, mat).</p>
+
+            <h3 id=\"potisk-vozidel\">Potisk vozidel</h3>
+            <p>Jednotné a vkusné označení vozidel společnosti silně podporuje <strong>povědomí</strong> veřejnosti <strong>o značce</strong> zejména v rámci jednoho regionu. Kromě <strong>logotypu</strong> se na vozidla z pravidla uvádějí nejvýznamnější <strong>kontaktní informace</strong> jako webová adresa nebo telefon, které však musí být dostatečně čitelné. Potisk se provádí pomocí barevných vyřezávaných folií nebo jednou folií, která je sama potištěna.</p>
+
+
+
+
+            <h2 id=\"flashova-grafika\">Interaktivní flashová grafika</h2>
+            <p>Připadají Vám Vaše webové stránky příliš obyčejné, chudé a nudné? Rádi byste animovaný banner pro internetovou reklamní kampaň, animované intro, slideshow (malá galerie, sada obrázků) nebo krátké video? Chcete <strong>zaujmout a rozptýlit</strong> návštěvníky webových stránek jednoduchou zábavnou hrou? Chcete nabídnout Vašim návštěvníkům možnost stáhnout si originální pozadí na plochu či spořič obrazovky s motivem Vaší firmy.</p>
+
+            <h3 id=\"bannery\">Bannery</h3>
+            <p>Pokud se chystáte upoutat pozornost na svůj web pomocí internetové reklamní kampaně, budete pravděpodobně potřebovat animované bannery, kterých si veřejnost <strong>všimne a zapamatuje</strong> si jejich <strong>sdělení</strong>.</p>
+
+            <h3 id=\"intra\">Intra</h3>
+            <p>Hravé stránky menšího rozsahu (tzv. microsites) je vhodné doplnit zajímavým intrem, které diváka <strong>upoutá</strong> a krátce ho <strong>seznámí</strong> s tématem stránek.</p>
+
+            <h3 id=\"obrazkove-galerie\">Obrázkové galerie</h3>
+            <p>Připadá vám klasický statický výčet fotografií na Vaší stránce příliš fádní? Chcete, aby obrázky zajímavě reagovali na přejetí myší nebo se otevírali pomocí <strong>nevtíravé animace</strong>? Proto je tu technologie flash, která Vám toto umožní.</p>
+
+            <h3 id=\"videa\">Videa</h3>
+            <p>Dnešní uživatelé jsou náročnější a očekávají více audiovizuálních informací než dříve. Jsou zvyklí používat malé <strong>videopřehrávače integrované do webu</strong> jako např. Youtube. Není pro nás problém vložit do vašich stránek krátké video s přehledným ovládáním.</p>
+
+            <h3 id=\"interaktivni-mapy\">Interaktivní mapy</h3>
+            <p>Potřebujete návštěvníky navést do různých regionů?. Má Vaše společnost více poboček? Moderním způsobem lze vytvořit interaktivní mapu <strong>reagující na pohyb kurzoru</strong> a zobrazující informace po kliknutí nebo sloužící jako rozcestník na další podstránky.</p>
+
+            <h3 id=\"intearaktivni-aplikace\">Interaktivní aplikace, hry</h3>
+            <p>Není vaším cílem vytvoření klasických webových stránek sice s dynamickým obsahem ale statickou formou? Máte vyšší požadavky na <strong>funkčnost a estetičnost</strong>? Chcete uživatelům nabídnout možnost zahrát si hru, která jim přiblíží Váš produkt?</p>
+
+            <h3 id=\"sporice-obrazovky\">Spořiče obrazovky</h3>
+            <p>Chcete připravit bonus pro uživatele? Umožněte jim stažení animovaného spořiče obrazovky <strong>s tématem</strong> vaší společnosti, služby či produktu.</p>
+
+
+
+
             <h2 id=\"grafika-3d\">3D grafika</h2>
             <p>Kde nestačí obrázky, fotky a jednoduché tvary, nastupuje třetí rozměr. Díky němu můžete mít prostorové logo, proletět se vesmírem a z&nbsp;plánů vyroste dům. Fantazii se meze nekladou. <strong>Pomocí moderních digitálních technologií je možné téměř vše.</strong> Typické použití nalezne v&nbsp;architektonických vizualizacích budoucích staveb. Může se jednat pouze o několik fiktivních záběrů, realistický zákres do fotografie nebo celé video, dokumentující exteriér i interiér budovy.</p>
             <p>V multimediálních projektech se často vyskytují různá abstraktní videa doplňující vizuální zážitek. 3D počítačovou grafiku lze například využít pro vytvoření maskota společnosti.</p>
+          
+          
+          
+          
           </div>".nl);
 
 ?>
