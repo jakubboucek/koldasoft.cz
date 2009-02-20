dcdc2da24d021ecd56521e3f80d4d876f5c4971a
kolda <kolda@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-02-20 20:48:50 +0000

    Doplnění obsahu.
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@173 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/nabidka-sluzeb/graficke-prace.php
+++ source/pages/nabidka-sluzeb/graficke-prace.php
@@ -8,16 +8,16 @@
 
 
             <h2 id=\"webdesign\">".mylink("webdesign")."</h2>
-            <p>Lorem Ipsum Dolr Sit Amed&hellip; ".mylink("webdesign", "čtěte dále")."</p>          
+            <p>Je vždy radost pohledět na pěknou webovou stránku s&nbsp;lehkou a osvěžující grafikou, která Vás <strong>zaujme</strong> a která Vám <strong>nebrání v&nbsp;orientaci</strong> na stránkách. Zvýší se tím pravděpodobnost dalších návštěv a zaručí Vám spokojené uživatele Vašich webových stránek. Už první pohled dokáže návštěvníka podvědomě naladit na co se má připravit&hellip; ".mylink("webdesign", "čtěte dále")."</p>                 
           
             <h2 id=\"corporate-design\">".mylink("corporate-design")."</h2>
-            <p>Lorem Ipsum Dolr Sit Amed&hellip; ".mylink("corporate-design", "čtěte dále")."</p>          
+            <p>Toto anglické slovní spojení znamená firemní grafický styl neboli &bdquo;tvář firmy&rdquo;, kterou se firma prezentuje na veřejnosti. Základem corporate designu je grafický manuál, který obsahuje logotyp (značku) firmy a popisuje, jak toto logo a prvky s&nbsp;ním související správně aplikovat v&nbsp;praxi a jak s&nbsp;ním naopak nezacházet&hellip; ".mylink("corporate-design", "čtěte dále")."</p>          
 
             <h2 id=\"tistena-grafika\">".mylink("tistena-grafika")."</h2>
-            <p>Lorem Ipsum Dolr Sit Amed&hellip; ".mylink("tistena-grafika", "čtěte dále")."</p>          
+            <p>Každá firma či organizace potřebuje nějaké tiskoviny. Nezáleží na tom, zda se jedná o ceníky a katalogy výrobků, ročenku o hospodaření, novoroční pozdrav obchodním partnerům, letáky, plakáty, trička, samolepky pro reklamní kampaně, brožury k&nbsp;produktům nebo prostě jen vizitky. Vše Vám zhotovíme dle Vašich přání a za dodržení Vašeho firemního grafického stylu&hellip; ".mylink("tistena-grafika", "čtěte dále")."</p>          
 
             <h2 id=\"interaktivni-flashova-grafika\">".mylink("interaktivni-flashova-grafika")."</h2>
-            <p>Lorem Ipsum Dolr Sit Amed&hellip; ".mylink("interaktivni-flashova-grafika", "čtěte dále")."</p>          
+            <p>Připadají Vám Vaše webové stránky příliš obyčejné, chudé a nudné? Rádi byste animovaný banner pro internetovou reklamní kampaň, animované intro, slideshow (malá galerie, sada obrázků) nebo krátké video? Chcete <strong>zaujmout a rozptýlit</strong> návštěvníky webových stránek jednoduchou zábavnou hrou? Chcete nabídnout Vašim návštěvníkům možnost stáhnout si originální pozadí na plochu či spořič obrazovky s&nbsp;motivem Vaší firmy&hellip; ".mylink("interaktivni-flashova-grafika", "čtěte dále")."</p>          
 
             <h2 id=\"grafika-3d\">3D grafika</h2>
             <p>Kde nestačí obrázky, fotky a jednoduché tvary, nastupuje třetí rozměr. Díky němu můžete mít prostorové logo, proletět se vesmírem a z&nbsp;plánů vyroste dům. Fantazii se meze nekladou. <strong>Pomocí moderních digitálních technologií je možné téměř vše.</strong> Typické použití nalezne v&nbsp;architektonických vizualizacích budoucích staveb. Může se jednat pouze o několik fiktivních záběrů, realistický zákres do fotografie nebo celé video, dokumentující exteriér i interiér budovy.</p>
