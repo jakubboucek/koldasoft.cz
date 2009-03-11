ae87fa57d68f1c95b64782f26b5fbc27c26df8a6
lucka <lucka@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2009-03-11 09:14:16 +0000

    upravena reference zdravotniho poradenstvi
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@189 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- source/pages/reference.php
+++ source/pages/reference.php
@@ -353,13 +353,12 @@
             <div class=\"reference\">
               <div class=\"text\">
                 <h3>Zdravotní poradentsví</h3>
-                <p class=\"link\"><a href=\"http://www.zdravotniporadenstvi.cz/\" title=\"Přejít na stránky Zdravotní poradenství\">www.zdravotniporadenstvi.cz</a></p>
                 <p class=\"short\"><strong>Kompletní realizace</strong>, únor&nbsp;2007</p>
                 <p class=\"long\">Jednoduché webové stránky nabízejí odborné poradenství v oblasti zdravotnictví. Cílem stránek je představit lékaře a jejich nabídku.</p>
                 <p>Webové stránky jsou vytvořeny ve dvou jazykových mutacích (česky, anglicky).</p>
               </div>
               <div class=\"foto\">
-                <p><a href=\"http://www.zdravotniporadenstvi.cz/\" title=\"Přejít na stránky Zdravotní poradenství\"><img src=\"".$cesta."reference/zdravotniporadenstvi_206.png\" alt =\"Náhled: projekt Zdravotní poradenství\" width=\"206\" height=\"206\" /></a></p>
+                <p><a href=\"".$cesta."reference/zdravotniporadenstvi.png\" title=\"Zobrazit velký náhled\"><img src=\"".$cesta."reference/zdravotniporadenstvi_206.png\" alt =\"Náhled: projekt Zdravotní poradenství\" width=\"206\" height=\"206\" /></a></p>
               </div>  
               <div class=\"cleaner\"></div>
             </div>
