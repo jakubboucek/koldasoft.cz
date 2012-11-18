c853c4b6901c783f5d23fc86021fd5c4791ec335
jakub.boucek <jakub.boucek@40ecb9ab-d4a2-264f-abd0-7c774a84dcac>
2012-11-18 15:15:13 +0000

    Commit current files
    git-svn-id: https://svn.bukajuv.net:8090/svn/koldasoft_cz/trunk@262 40ecb9ab-d4a2-264f-abd0-7c774a84dcac

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cz_koldasoft');
+define('DB_NAME', 'koldasoft');
 
 /** MySQL database username */
-define('DB_USER', 'koldasoft.cz');
+define('DB_USER', 'koldasoft');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Rg3hb3hP');
+define('DB_PASSWORD', 'sna_cess');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql01.mam-web.cz');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
